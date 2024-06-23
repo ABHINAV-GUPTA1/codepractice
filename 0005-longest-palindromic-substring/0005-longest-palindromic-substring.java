26404834f4aeed46e3dbb38956fa07dc314db248
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-06-23 16:40:54 +0530

    Time: 15 ms (88.28%), Space: 42.1 MB (82.76%)

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int left = 0;
+        int right = 0;
+        int start = 0;
+        int maxlen = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            left = i;
+            right = i + 1;
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            }
+            left++;
+            right--;
+            if (left >= 0 && right < n && maxlen < right - left + 1) {
+                maxlen = right - left + 1;
+                start = left;
+            }
+
+            left = i - 1;
+            right = i + 1;
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            }
+            left++;
+            right--;
+            if (left >= 0 && right < n && maxlen < right - left + 1) {
+                maxlen = right - left + 1;
+                start = left;
+            }
+        }
+        return s.substring(start, start+maxlen);
+    }
+}
