953252d340198840163e794fc116bd60b892dc6d
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-06-23 18:59:12 +0530

    Time: 3 ms (95.43%), Space: 43.8 MB (78.69%)

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,6 +2,38 @@ class Solution {
 
     public int lengthOfLIS(int[] nums) { 
         int n = nums.length;
+        int idx = 0;
+        int[] temp = new int[n];
+        temp[idx++] = nums[0];
+        int k = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > temp[idx-1]) {
+                temp[idx++] = nums[i];
+            } else {
+                k = lowerBound(temp, 0, idx-1, nums[i]);
+                temp[k] = nums[i];
+            }
+        }
+        return idx;
+    }
+
+    int lowerBound(int[] arr, int l, int r, int target) {
+        int mid;
+        int ans = 0;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public int lengthOfLIS2(int[] nums) { 
+        int n = nums.length;
         int[] dp = new int[n];
         int ans = 0;
         for (int i = 0; i < n; i++) {
