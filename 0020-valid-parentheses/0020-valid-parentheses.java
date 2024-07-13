12d1b3d0ce1e8cefcdbc1aedd9c245418564586e
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-07-13 15:43:54 +0530

    Time: 2 ms (54.29%), Space: 41.4 MB (55.69%)

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
+            } else {
+                if (st.isEmpty() || !(isValid(st.peek(), s.charAt(i)))) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+
+    private boolean isValid(char a, char b) {
+        return (a == '(' && b == ')') || (a == '{' && b == '}') || (a == '[' && b == ']');
+    }
+}
