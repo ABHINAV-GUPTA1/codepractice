e8d3d45f96a1c593841817f31ee291db6e30104f
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-07-14 11:43:35 +0530

    Time: 0 ms (100%), Space: 40.2 MB (53.24%)

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,27 @@
+class Solution {
+    int dp[];
+    public int climbStairs(int n) {
+        // dp = new int[n+1];
+        // return f(n);
+
+        dp = new int[n+1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+
+    int f(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        return dp[n] = f(n-1) + f(n-2);
+    }
+}
