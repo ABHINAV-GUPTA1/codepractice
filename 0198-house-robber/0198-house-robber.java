3de57dee44c2ac98a8bddcbe7631d02ff7a7472e
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-07-14 13:18:16 +0530

    Time: 0 ms (100%), Space: 40.9 MB (34.16%)

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,45 @@
+class Solution {
+    int[] dp;
+    public int rob(int[] nums) {
+        dp = new int[nums.length+1];
+        Arrays.fill(dp, -1);
+        // return f1(0, nums);
+        // return f2(nums.length-1, nums);
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2; i <= nums.length; i++) {
+            dp[i] = Math.max(nums[i-1]+dp[i-2], dp[i-1]);
+        }
+
+        return dp[nums.length];
+    }
+
+    int f2(int n, int[] arr) {
+        if (n == 0) {
+            return arr[n];
+        }
+        if (n < 0) {
+            return 0;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = Math.max(arr[n] + f2(n-2, arr), f2(n-1, arr));
+    }
+
+
+    int f1(int n, int[] nums) {
+        if (n >= nums.length) {
+            return 0;
+        }
+        if (n == nums.length-1) {
+            return nums[n];
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = Math.max(nums[n] + f1(n+2, nums), f1(n+1, nums));
+    }
+}
