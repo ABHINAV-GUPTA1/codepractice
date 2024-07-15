038197dbdad2ccf260ef867c1dc85b1c6b12b03c
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-07-15 12:17:21 +0530

    Time: 127 ms (5.3%), Space: 58.2 MB (5.96%)

---
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, Integer> ans;
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        TreeNode root;
+        Map<Integer, ArrayList<Integer>> childParentMap = new HashMap<>();
+        Map<Integer, TreeNode> map = new HashMap<>();
+        TreeNode k =  new TreeNode();
+        for (int[] d : descriptions) {
+            k = map.getOrDefault(d[0], new TreeNode(d[0]));
+            if (d[2] == 0) {
+                k.right = map.getOrDefault(d[1], new TreeNode(d[1]));
+                map.put(d[1], k.right);
+            } else {
+                k.left = map.getOrDefault(d[1], new TreeNode(d[1]));
+                map.put(d[1], k.left);
+            }
+            
+            map.put(d[0], k);            
+            childParentMap.computeIfAbsent(d[0], x->new ArrayList<>()).add(d[1]);
+        }   
+        int res = 0;
+        ans = new HashMap<>(); 
+        int kval;
+        for (Integer k1 : childParentMap.keySet()) {
+            kval = f(k1, childParentMap);
+            if (res < kval) {
+                res = kval;
+                k = map.get(k1);
+            }
+        }
+        
+        return k;
+    }
+
+    int f(int key, Map<Integer, ArrayList<Integer>> m2) {
+        if (!m2.containsKey(key)) {
+            return 0;
+        }
+        int count = 0;
+        if (ans.containsKey(key)) {
+            return ans.get(key);
+        }
+        for (Integer k: m2.get(key)) {
+            count += f(k, m2);
+        }
+        ans.put(key, m2.get(key).size() + count);
+        return ans.get(key);
+    }
+    
+}
