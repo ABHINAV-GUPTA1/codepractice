10544ac9c243a2c5fc55434fc4c5b5a8c880a9bf
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-07-20 15:10:49 +0530

    Time: 2 ms (93.89%), Space: 45.2 MB (33.38%)

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
@@ -1,25 +1,24 @@
 class Solution {
     public List<Integer> luckyNumbers (int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
         int m = matrix.length;
         int n = matrix[0].length;
-        int[] row = new int[m];
-        int[] col = new int[n];
-        int k = 0;
-
-        List<Integer> res = new ArrayList<>();
-
+        int row[] = new int[m];
+        int col[] = new int[n];
         for (int i = 0; i < m; i++) {
             row[i] = Integer.MAX_VALUE;
             for (int j = 0; j < n; j++) {
                 row[i] = Math.min(row[i], matrix[i][j]);
             }
         }
+
         for (int i = 0; i < n; i++) {
             col[i] = Integer.MIN_VALUE;
             for (int j = 0; j < m; j++) {
                 col[i] = Math.max(col[i], matrix[j][i]);
             }
         }
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (row[i] == matrix[i][j] && col[j] == matrix[i][j]) {
@@ -27,7 +26,6 @@ class Solution {
                 }
             }
         }
-
         return res;
     }
 }
