493a6a84184466a86c0d5fb2358656274be0a204
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-07-20 09:47:33 +0530

    Time: 1 ms (100%), Space: 52.7 MB (85.26%)

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int m = rowSum.length;
+        int n = colSum.length;
+        int[][] res = new int[m][n];
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            res[i][j] = Math.min(rowSum[i], colSum[j]);
+            rowSum[i] -= res[i][j];
+            colSum[j] -= res[i][j];
+            if (rowSum[i] == 0) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return res;
+    }
+}
