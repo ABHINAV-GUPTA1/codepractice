305910b10e246c2055c2702f73974ddd6f9df3b8
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-07-26 13:51:34 +0530

    Time: 1 ms (100%), Space: 47.1 MB (53.26%)

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -3,20 +3,19 @@ class Solution {
         int n = nums.length;
         int i = 0;
         int j = n-1;
-        int k = n-1;
         int[] ans = new int[n];
-        int v1,v2;
-        while (k >= 0) {
+        int k = n-1;
+        int v1, v2;
+        while (i <= j) {
             v1 = nums[i]*nums[i];
-            v2 = nums[j] * nums[j];
+            v2 = nums[j]*nums[j];
             if (v1 > v2) {
-                ans[k] = v1;
+                ans[k--] = v1;
                 i++;
             } else {
-                ans[k] = v2;
+                ans[k--] = v2;
                 j--;
             }
-            k--;
         }
         return ans;
     }
