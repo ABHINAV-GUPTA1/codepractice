6d5d60038cef4d102a8c40bea3956f0810507399
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-07-26 20:00:48 +0530

    Time: 96 ms (42.05%), Space: 45.3 MB (5.38%)

--- 0869-similar-string-groups/0869-similar-string-groups.java
+++ 0869-similar-string-groups/0869-similar-string-groups.java
@@ -1,5 +1,49 @@
 class Solution {
+    
+    int[] parent;
+    int[] rank;
+    private int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    private void union(int u, int v) {
+        int ux = find(u);
+        int vx = find(v);
+        if (ux == vx) {
+            return;
+        }
+        if (rank[ux] < rank[vx]) {
+            parent[ux] = vx; 
+            rank[vx] += 1;
+        } else {
+            parent[vx] = ux; 
+            rank[ux] += 1;
+        }
+    }
+
     public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        int ans = n;
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (isSimilar(strs[i], strs[j]) && find(i) != find(j)) {
+                    ans--;
+                    union(i, j);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int numSimilarGroups1(String[] strs) {
         Map<Integer, List<Integer>> map = new HashMap<>();
         int n = strs.length;
         int ans = 0;
