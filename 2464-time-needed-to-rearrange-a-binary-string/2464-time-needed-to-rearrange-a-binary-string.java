009cf4999deb231884a5d7d96db3f49e437790f3
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-01 10:51:23 +0530

    Time: 25 ms (67.84%), Space: 41.8 MB (57.71%)

--- 2464-time-needed-to-rearrange-a-binary-string/2464-time-needed-to-rearrange-a-binary-string.java
+++ 2464-time-needed-to-rearrange-a-binary-string/2464-time-needed-to-rearrange-a-binary-string.java
@@ -1,22 +1,23 @@
 class Solution {
     public int secondsToRemoveOccurrences(String s) {
-        int count = 0;
         char[] arr = s.toCharArray();
+        int n  = arr.length;
+        int ans = 0;
+        boolean flag;
         while (true) {
-            boolean flag = false;
-            for (int i = 1; i < arr.length; i++) {
+            flag = false;
+            for (int i = 1; i < n; i++) {
                 if (arr[i-1] == '0' && arr[i] == '1') {
+                    flag = true;
                     arr[i-1] = '1';
                     arr[i] = '0';
-                    flag = true;
                     i++;
                 }
             }
             if (!flag) {
-                break;
+                return ans;
             }
-            count++;
+            ans++;
         }    
-        return count;
     }
 }
