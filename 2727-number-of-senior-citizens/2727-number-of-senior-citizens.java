c039c644245bee15696e1ef8ced750c6bdcaa5f9
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-01 10:13:45 +0530

    Time: 0 ms (100%), Space: 42.1 MB (89.84%)

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -1,8 +1,16 @@
 class Solution {
     public int countSeniors(String[] details) {
         int count = 0;
+        int c1;
+        int c2;
+        int age;
         for (String s : details) {
-            count += (Integer.parseInt(s.substring(11, s.length()-2))) > 60 ? 1 : 0;
+            c1 = s.charAt(11) - '0';
+            c2 = s.charAt(12) - '0';
+            age = c1*10+c2;
+            if (age > 60) {
+                count++;
+            }
         }    
 
         return count;
