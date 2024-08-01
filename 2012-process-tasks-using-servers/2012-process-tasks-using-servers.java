ee7685707b8cbd8d40feaef3fa5fd8fea14e13b6
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-01 10:40:01 +0530

    Time: 235 ms (36.51%), Space: 76.8 MB (60.84%)

--- 2012-process-tasks-using-servers/2012-process-tasks-using-servers.java
+++ 2012-process-tasks-using-servers/2012-process-tasks-using-servers.java
@@ -1,46 +1,42 @@
 class Solution {
     public int[] assignTasks(int[] servers, int[] tasks) {
-        int tn = tasks.length;
-        int[] res = new int[tn];
+        int n = tasks.length;
+        int[] res = new int[n];
         PriorityQueue<int[]> availPq = new PriorityQueue<>((a,b)-> {
-            if (a[0]==b[0]) {
-                return a[1]-b[1];
-            }
-            return a[0]-b[0];
-        });
-
-        PriorityQueue<int[]> unavailPq = new PriorityQueue<>((a,b)-> {
             if (a[0] == b[0]) {
-                if (a[1] == b[1]) {
-                    return a[2]-b[2];
-                }
-                return a[1]-b[1];
+                return a[1] - b[1];
             }
             return a[0] - b[0];
         });
-
+        PriorityQueue<int[]> unavailPq = new PriorityQueue<>((a,b)->{
+            if (a[0] == b[0]) {
+                if (a[1] == b[1]) {
+                    return a[2] - b[2];
+                }
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        });
         for (int i = 0; i < servers.length; i++) {
             availPq.offer(new int[]{servers[i], i});
         }
         int t = 0;
-        for (int i = 0; i < tasks.length; i++) {
+        int val[];
+        for (int i = 0; i < n; i++) {
             t = Math.max(t, i);
-            int[] server;
             while (!unavailPq.isEmpty() && unavailPq.peek()[0] <= i) {
-                server = unavailPq.poll();
-                availPq.offer(new int[]{server[1], server[2]});
+                val = unavailPq.poll();
+                availPq.offer(new int[]{val[1], val[2]});
             }
             if (availPq.isEmpty()) {
-                server = unavailPq.poll();
-                if (server[0] > t) {
-                    t = server[0];
-                }
-                res[i] = server[2];
-                unavailPq.offer(new int[]{t+tasks[i], server[1], server[2]});
+                val = unavailPq.poll();
+                t = val[0];
+                res[i] = val[2];
+                unavailPq.offer(new int[]{t+tasks[i], val[1], val[2]});
             } else {
-                server = availPq.poll();
-                res[i] = server[1];
-                unavailPq.offer(new int[]{t+tasks[i], server[0], server[1]});
+                val = availPq.poll();
+                res[i] = val[1];
+                unavailPq.offer(new int[]{t+tasks[i], val[0], val[1]});
             }
         }
 
