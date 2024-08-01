1e421ea8a5406130c9159ed32191fe052d29153e
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-01 10:48:43 +0530

    Time: 12 ms (40.61%), Space: 52.8 MB (90.13%)

--- 0881-loud-and-rich/0881-loud-and-rich.java
+++ 0881-loud-and-rich/0881-loud-and-rich.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] loudAndRich(int[][] richer, int[] quiet) {
-        int[] indeg = new int[quiet.length];
-        return bfs(createAdj(richer, indeg), indeg, quiet);
-    }
-
-    private int[] bfs(Map<Integer, List<Integer>> adj, int[] indeg, int[] quiet) {
-        int n = indeg.length;
-        int[] res = new int[n];
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        int n = quiet.length;
+        int[] indegree = new int[n];
+        for (int i = 0; i < richer.length; i++) {
+            adj.computeIfAbsent(richer[i][0], x->new ArrayList<>()).add(richer[i][1]);
+            indegree[richer[i][1]]++;
+        }
         Queue<Integer> q = new LinkedList<>();
+        int res[]  = new int[n];
         for (int i = 0; i < n; i++) {
             res[i] = i;
-            if (indeg[i] == 0) {
+            if (indegree[i] == 0) {
                 q.offer(i);
             }
         }
-
         while (!q.isEmpty()) {
             int u = q.poll();
             if (adj.get(u) == null) {
@@ -24,21 +24,12 @@ class Solution {
                 if (quiet[res[v]] > quiet[res[u]]) {
                     res[v] = res[u];
                 }
-                if (--indeg[v] == 0) {
+                if (--indegree[v] == 0) {
                     q.offer(v);
                 }
             }
         }
 
         return res;
-    }
-
-    private Map<Integer, List<Integer>> createAdj(int[][] rich, int[] indeg) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < rich.length; i++) {
-            map.computeIfAbsent(rich[i][0], x->new ArrayList<>()).add(rich[i][1]);
-            indeg[rich[i][1]]++;
-        }
-        return map;
     }
 }
