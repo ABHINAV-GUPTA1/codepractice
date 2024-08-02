0c8cc3ae348e1dff9d8d8cf186dd50ad1eb44c16
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-02 13:20:13 +0530

    Time: 0 ms (100%), Space: 40.8 MB (69.02%)

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,6 +2,25 @@ class Solution {
     int[] dp;
     
     public int rob(int[] nums) {
+        int prev1 = nums[0];
+        int prev2 = 0;
+        int curr;
+        int n = nums.length;
+        for (int idx = 1; idx < n; idx++) {
+            int take = nums[idx]; 
+            if (idx > 1) {
+                take += prev2;
+            }
+            int ntake = prev1;
+            curr = Math.max(take, ntake);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+    public int rob2(int[] nums) {
         int n = nums.length;
         dp = new int[n];
         dp[0] = nums[0];
