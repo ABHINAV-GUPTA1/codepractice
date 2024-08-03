d01e7886ef5a8904437f55311d2a4be1271fb39e
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-03 18:42:58 +0530

    Time: 4 ms (54.32%), Space: 44.1 MB (73.75%)

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,19 +1,28 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        // Map<Integer, Integer> freq = new HashMap<>();
+        // for (int i = 0; i < arr.length; i++) {
+        //     freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
+        // }        
+        // for (int i = 0; i < target.length; i++) {
+        //     if (!freq.containsKey(target[i])) {
+        //         return false;
+        //     }
+        //     freq.put(target[i], freq.get(target[i]) - 1);
+        //     if (freq.get(target[i]) == 0) {
+        //         freq.remove(target[i]);
+        //     }
+        // }
+
+        // return freq.isEmpty();
+
+        Arrays.sort(arr);
+        Arrays.sort(target);
         for (int i = 0; i < arr.length; i++) {
-            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
-        }        
-        for (int i = 0; i < target.length; i++) {
-            if (!freq.containsKey(target[i])) {
+            if (arr[i] != target[i]) {
                 return false;
             }
-            freq.put(target[i], freq.get(target[i]) - 1);
-            if (freq.get(target[i]) == 0) {
-                freq.remove(target[i]);
-            }
         }
-
-        return freq.isEmpty();
+        return true;
     }
 }
