d95e5193b614519d9807f710c0d985bc22c9750e
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-03 18:41:45 +0530

    Time: 8 ms (23.09%), Space: 44.4 MB (24.11%)

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
+        }        
+        for (int i = 0; i < target.length; i++) {
+            if (!freq.containsKey(target[i])) {
+                return false;
+            }
+            freq.put(target[i], freq.get(target[i]) - 1);
+            if (freq.get(target[i]) == 0) {
+                freq.remove(target[i]);
+            }
+        }
+
+        return freq.isEmpty();
+    }
+}
