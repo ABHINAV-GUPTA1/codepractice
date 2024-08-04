0149213641bf9558d101b1aafd1efd08c0690336
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-04 12:47:32 +0530

    Time: 1 ms (100%), Space: 47.5 MB (7.42%)

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,22 +1,32 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        
         int n = nums.length;
+        if (n < 1) {
+            return nums;
+        }
+        int[] res = new int[n];
+
         int i = 0;
         int j = n-1;
-        int[] ans = new int[n];
         int k = n-1;
-        int v1, v2;
+        int v1 = nums[i]*nums[i];
+        int v2 = nums[j]*nums[j];
         while (i <= j) {
-            v1 = nums[i]*nums[i];
-            v2 = nums[j]*nums[j];
             if (v1 > v2) {
-                ans[k--] = v1;
+                res[k--] = v1;
                 i++;
+                if (i <= j) {
+                    v1 = nums[i] * nums[i];
+                }
             } else {
-                ans[k--] = v2;
+                res[k--] = v2;
                 j--;
+                if (i <= j) {
+                    v2 = nums[j]*nums[j];
+                }
             }
         }
-        return ans;
+        return res;
     }
 }
