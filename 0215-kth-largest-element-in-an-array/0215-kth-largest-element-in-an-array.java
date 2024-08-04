c3d3e251941772ef02e48d66d0186a6730d87b04
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-04 11:53:42 +0530

    Time: 5 ms (96.79%), Space: 57.2 MB (74.58%)

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,7 +1,17 @@
 class Solution {
+    Random r;
     public int findKthLargest(int[] nums, int k) {
+        r = new Random();
         return quick(0, nums.length-1, nums, k);
     }
+    
+    void shuffle (int arr[], int low, int high) {
+        int ridx = r.nextInt(high-low+1)+low;
+        int t = arr[ridx];
+        arr[ridx] = arr[low];
+        arr[low] = t;
+    }
+
     int quick(int low, int high, int[] arr, int k) {
         while (low <= high) {
             int part = findPart(low, high, arr);
@@ -19,20 +29,36 @@ class Solution {
     }
 
     int findPart(int low, int high, int[] arr) {
+        shuffle(arr, low, high);
         int part = arr[low];
         int i = low+1;
         int j = high;
 
         while (i <= j) {
-            if (part <= arr[i]) {
-                i++;
-            } else if (part >= arr[j]) {
-                j--;
-            } else {
+            // if (part <= arr[i]) {
+            //     i++;
+            // } else if (part >= arr[j]) {
+            //     j--;
+            // } else {
+            //     int t = arr[i];
+            //     arr[i] = arr[j];
+            //     arr[j] = t;
+            //     i++;
+            //     j--;
+            // }
+
+            if (arr[i] < part && arr[j] > part) {
                 int t = arr[i];
                 arr[i] = arr[j];
                 arr[j] = t;
                 i++;
+                j--;
+            }
+
+            if (arr[i] >= part) {
+                i++;
+            }
+            if (arr[j] <= part) {
                 j--;
             }
         }
