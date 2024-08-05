be11e13c8493b0567458a6bebc37a20ad9fa6295
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-05 13:03:27 +0530

    Time: 0 ms (100%), Space: 44.5 MB (24.23%)

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,19 +11,16 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
         ListNode slow = head;
         ListNode fast = head;
-
         while (fast != null && fast.next != null) {
-            fast = fast.next.next;
             slow = slow.next;
-            if (slow == fast) {
+            fast = fast.next.next;
+            if (slow  == fast) {
                 return true;
             }
         }
+
         return false;
     }
 }
