21b662ab74e1c1b800bef68af936e4c7740f9a6c
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-09 18:38:50 +0530

    Time: 12 ms (80.28%), Space: 48.2 MB (71.12%)

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-        }
-        List<Integer> tlist[] = new List[n+1];
-        for (Map.Entry<Integer, Integer> me : map.entrySet()) {
-            if (tlist[me.getValue()] == null) {
-                tlist[me.getValue()] = new ArrayList<>();
-            }
-            tlist[me.getValue()].add(me.getKey());
-        }
         int[] res = new int[k];
         int idx = 0;
-        for (int i = n; i >= 0 && k > 0; i--) {
-            if (tlist[i] == null) {
-                continue;
-            }
-            for (int val : tlist[i]) {
-                res[idx++] = val;
-                k--;
-                if (k == 0) {
-                    return res;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+        List<Integer>[] freq = new List[nums.length+1];
+        for (int i = 0; i <= nums.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        for (Map.Entry<Integer, Integer> me : map.entrySet()) {
+            freq[me.getValue()].add(me.getKey());
+        }
+        for (int i = nums.length; i >= 0 && idx < k; i--) {
+            if (freq[i].size() > 0) {
+                for (int j = 0; j < freq[i].size() && idx < k; j++) {
+                    res[idx++] = freq[i].get(j);
                 }
             }
         }
+
         return res;
     }
 }
