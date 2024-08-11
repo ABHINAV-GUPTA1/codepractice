3ace3ab24ebbe64071350a5f8315291dcd886891
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-11 15:35:42 +0530

    Time: 7 ms (42.55%), Space: 45.3 MB (35.41%)

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,5 +1,32 @@
 class Solution {
+    
     public int[] asteroidCollision(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (st.isEmpty() || arr[i] > 0) {
+                st.push(i);
+            } else {
+                int val = Math.abs(arr[i]);
+                while (!st.isEmpty() && arr[st.peek()] < val && arr[st.peek()] > 0) {
+                    st.pop();
+                }
+                if (!st.isEmpty() && arr[st.peek()] == val && arr[st.peek()] > 0) {
+                    st.pop();
+                } else if (st.isEmpty() || arr[st.peek()] < 0) {
+                    st.push(i);
+                }
+            }
+
+        }
+        int n = st.size();
+        int[] res = new int[n];
+        while (!st.isEmpty()) {
+            res[--n] = arr[st.pop()];
+        }
+        return res;
+    }
+    
+    public int[] asteroidCollision1(int[] arr) {
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < arr.length; i++) {
             if (st.isEmpty()) {
