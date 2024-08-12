2a455e399e0cc65504385939060e8a691319cfd1
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-12 16:42:37 +0530

    Time: 9 ms (7.2%), Space: 49.2 MB (48.15%)

---
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int totalSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+        }
+        int maxSum = maxSubarray(nums);
+        int minSum = minSubarray(nums);
+
+        if (maxSum > 0) {
+            return Math.max(maxSum, totalSum - minSum);
+        }
+        return maxSum;
+    }
+
+    private int maxSubarray(int[] arr) {
+        int sum = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            sum = Math.max(arr[i]+sum, arr[i]);
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+
+    private int minSubarray(int[] arr) {
+        int sum = arr[0];
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            sum = Math.min(arr[i]+sum, arr[i]);
+            min = Math.min(min, sum);
+        }
+        return min;
+    }
+}
