3b4135fff9d37789e09f18468dd335f0596791d5
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-12 13:22:47 +0530

    Time: 2 ms (82.51%), Space: 43.7 MB (56.62%)

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,6 +1,20 @@
 class Solution {
     int[][] dp;
     public int maxProfit(int k, int[] prices) {
+        dp = new int[2*k+1][prices.length+1];
+        
+        for (int idx = prices.length-1; idx >= 0; idx--) {
+            for (int trans = 2*k-1; trans >= 0; trans--) {
+                if (trans%2 == 0) {
+                    dp[trans][idx] = Math.max(-prices[idx]  + dp[trans+1][idx+1], dp[trans][idx+1]);
+                } else {
+                    dp[trans][idx] = Math.max(prices[idx]  + dp[trans+1][idx+1], dp[trans][idx+1]);          }
+            }
+        }
+        return dp[0][0];
+    }
+
+    public int maxProfit1(int k, int[] prices) {
         dp = new int[2*k+1][prices.length];
         for (int i = 0; i <= 2*k; i++) {
             Arrays.fill(dp[i], -1);
