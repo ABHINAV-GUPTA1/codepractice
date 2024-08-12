1e1a814835af6e770eff05970132eb40da83c2c7
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-12 16:52:17 +0530

    Time: 7 ms (31.56%), Space: 49.1 MB (48.15%)

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -2,34 +2,25 @@ class Solution {
     int totalSum;
     public int maxSubarraySumCircular(int[] nums) {
         totalSum = 0;
-        int maxSum = maxSubarray(nums);
-        int minSum = minSubarray(nums);
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        totalSum = nums[0];
+        int msum = nums[0]; // max temp sum
+        int misum = nums[0]; // min temp sum
+        for (int i = 1; i < nums.length; i++) {
+            totalSum += nums[i];
+            msum = Math.max(nums[i]+msum, nums[i]);
+            maxSum = Math.max(maxSum, msum);
+
+            misum = Math.min(nums[i]+misum, nums[i]);
+            minSum = Math.min(minSum, misum);
+
+        }
+
 
         if (maxSum > 0) {
             return Math.max(maxSum, totalSum - minSum);
         }
         return maxSum;
-    }
-
-    private int maxSubarray(int[] arr) {
-        int sum = arr[0];
-        int max = arr[0];
-        totalSum = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            totalSum += arr[i];
-            sum = Math.max(arr[i]+sum, arr[i]);
-            max = Math.max(max, sum);
-        }
-        return max;
-    }
-
-    private int minSubarray(int[] arr) {
-        int sum = arr[0];
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            sum = Math.min(arr[i]+sum, arr[i]);
-            min = Math.min(min, sum);
-        }
-        return min;
     }
 }
