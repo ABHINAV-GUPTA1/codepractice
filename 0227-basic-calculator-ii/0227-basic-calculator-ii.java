922b500dfc96f8dc846c6abd1941bb7b339d8b3b
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-12 12:18:41 +0530

    Time: 23 ms (57.38%), Space: 46.7 MB (22.55%)

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int calculate(String s) {
+        int num = 0;
+        Stack<Integer> st = new Stack<>();
+        char sign = '+';
+        for (int i = 0; i < s.length(); i++) {
+            boolean isDigit = s.charAt(i) >='0' && s.charAt(i) <= '9';
+            if (isDigit) {
+                num = num * 10 + (s.charAt(i)-'0');
+            }
+            if ((!isDigit && s.charAt(i) != ' ') || i == s.length()-1) {
+                if (sign == '+') {
+                    st.push(num);
+                } else if (sign == '-') {
+                    st.push(-num);
+                } else if (sign == '*') {
+                    st.push(st.pop() * num);
+                } else if (sign == '/') {
+                    st.push(st.pop() / num);
+                }
+                num = 0;
+                sign = s.charAt(i);
+            }
+        }
+        int res = 0;
+        while (!st.isEmpty()) {
+            res += st.pop();
+        }    
+
+        return res;
+    }
+}
