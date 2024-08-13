863033294ea550724ddb3d409bb20bcc152a7ad8
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-13 10:55:04 +0530

    Time: 2 ms (84.16%), Space: 44.6 MB (48.23%)

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,22 +1,26 @@
 class Solution {
+    List<List<Integer>> res;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        f(0, candidates, target, new ArrayList<>(), res);
+        res = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(0, candidates, target, new ArrayList<>());
         return res;
     }
 
-    private void f(int idx, int[] arr, int target, List<Integer> tmp, List<List<Integer>> res) {
+    private void backtrack(int idx, int[] arr, int target, List<Integer> tmp) {
         if (target == 0) {
             res.add(new ArrayList<>(tmp));
             return;
         }
-        if (idx >= arr.length) {
+        if (idx >= arr.length || target < 0) {
             return;
         }
-        f(idx+1, arr, target, tmp, res);
-        if (target >= arr[idx]) {
+
+        backtrack(idx+1, arr, target, tmp);
+
+        if (target-arr[idx] >= 0) {
             tmp.add(arr[idx]);
-            f(idx, arr, target-arr[idx], tmp, res);
+            backtrack(idx, arr, target-arr[idx], tmp);
             tmp.remove(tmp.size()-1);
         }
     }
