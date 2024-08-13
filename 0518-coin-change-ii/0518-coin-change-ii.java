1ee30ee7099864e00d1c320071d1cec10a00affa
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-13 12:27:08 +0530

    Time: 5 ms (70.61%), Space: 47.8 MB (41.24%)

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,28 +1,29 @@
 class Solution {
     int[][] dp;
     public int change(int amount, int[] coins) {
-        dp = new int[coins.length][amount+1];
-        for (int i = 0; i < coins.length; i++) {
+        dp = new int[coins.length+1][amount+1];
+        for (int i = 0; i <= coins.length; i++) {
             Arrays.fill(dp[i], -1);
         }
-        return f(coins.length-1, coins, amount);
+        return f(coins.length-1, amount, coins);
     }
 
-    int f(int idx, int[] coins, int amt) {
+    int f(int idx, int target, int[] arr) {
         if (idx == 0) {
-            if (amt%coins[idx] == 0) {
-                return 1;
-            }
+            return target%arr[idx] == 0 ? 1 : 0;
+        }
+        if (target < 0) {
             return 0;
         }
-        if (dp[idx][amt] != -1) {
-            return dp[idx][amt];
+        if (dp[idx][target] != -1) {
+            return dp[idx][target];
         }
-        int ntake = f(idx-1, coins, amt);
+        int ntake = f(idx-1, target, arr);
         int take = 0;
-        if (coins[idx] <= amt) {
-            take = f(idx, coins, amt-coins[idx]);
+        if (target-arr[idx] >= 0) {
+            take = f(idx, target-arr[idx], arr);
         }
-        return dp[idx][amt] = take+ntake;
+
+        return dp[idx][target] = take+ntake;
     }
 }
