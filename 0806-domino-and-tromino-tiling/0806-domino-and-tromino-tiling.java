9332cfee2a00935a55c03c92143e3465b153cd59
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-14 11:54:48 +0530

    Time: 0 ms (100%), Space: 40.5 MB (36.34%)

---
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -0,0 +1,25 @@
+class Solution {
+    int MOD = (int)1e9+7;
+    public int numTilings(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (n == 3) {
+            return 5;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        int ans = 0;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = ((2*dp[i-1])%MOD + dp[i-3])%MOD;
+        }
+
+        return dp[n];
+    }
+}
