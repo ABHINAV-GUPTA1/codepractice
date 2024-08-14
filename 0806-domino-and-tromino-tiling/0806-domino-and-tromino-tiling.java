8705c34235772676241aa91ed5510d9560a63037
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-14 11:59:04 +0530

    Time: 0 ms (100%), Space: 40.3 MB (67.13%)

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -10,16 +10,24 @@ class Solution {
         if (n == 3) {
             return 5;
         }
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
+        // int[] dp = new int[n+1];
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // dp[2] = 2;
+        // dp[3] = 5;
+
+        int a = 1;
+        int b = 1;
+        int c = 2;
         int ans = 0;
         for (int i = 3; i <= n; i++) {
-            dp[i] = ((2*dp[i-1])%MOD + dp[i-3])%MOD;
-        }
+            // dp[i] = ((2*dp[i-1])%MOD + dp[i-3])%MOD;
 
-        return dp[n];
+            ans = ((2*c)%MOD + a)%MOD;
+            a = b;
+            b = c;
+            c = ans;
+        }
+        return c;
     }
 }
