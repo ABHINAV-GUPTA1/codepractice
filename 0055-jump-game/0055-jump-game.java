cbfb0b1b01bac84d6abc506c2ea51934db93329e
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-14 12:03:16 +0530

    Time: 2 ms (78.01%), Space: 46.1 MB (8.21%)

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int max = 0;
-        for (int i = 0; i < nums.length && max >= i; i++) {
-            if (max < i + nums[i]) {
-                max = i + nums[i];
+        for (int i = 0; i < nums.length && i <= max; i++) {
+            if (nums[i]+i > max) {
+                max = nums[i] + i;
             }
             if (max >= nums.length-1) {
                 return true;
