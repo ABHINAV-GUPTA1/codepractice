aa5df68861398c3d723134d1be1487deb357afca
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-17 14:18:04 +0530

    Time: 280 ms (11.18%), Space: 153.4 MB (12.35%)

--- 2577-count-palindromic-subsequences/2577-count-palindromic-subsequences.java
+++ 2577-count-palindromic-subsequences/2577-count-palindromic-subsequences.java
@@ -1,15 +1,62 @@
 class Solution {
     int[][][][] dp;
     public int countPalindromes(String s) {
-        dp = new int[s.length()+1][10][10][5];
+        dp = new int[s.length()+1][11][11][6];
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    dp[i][j][k][5] = 1;
+                }
+            }
+        }
+        String str = s;
+
+        // return (int)(dfs(0, 10, 10, 0, s)%mod);
+
+
+        for (int strInd = s.length()-1; strInd >= 0; strInd--) {
+            for (int first = 0; first < 10; first++) {
+                for (int second = 0; second < 10; second++) {
+                    for (int newStrInd = 4; newStrInd >= 0; newStrInd--) {
+                        int nPick = dp[strInd+1][first][second][newStrInd]%mod;
+
+                        int pick = 0;
+                        if (newStrInd == 0) {
+                            // pick = dfs(strInd+1, str.charAt(strInd)-'0', second, newStrInd+1, str)%mod;
+                            pick = dp[strInd+1][str.charAt(strInd)-'0'][second][newStrInd+1]%mod;
+                        } else if (newStrInd == 1) {
+                            // pick = dfs(strInd+1, first, str.charAt(strInd)-'0', newStrInd+1, str)%mod;
+                            pick = dp[strInd+1][first][str.charAt(strInd)-'0'][newStrInd+1]%mod;
+                        } else if (newStrInd == 2) {
+                            pick = dp[strInd+1][first][second][newStrInd+1]%mod;
+                        } else if (newStrInd == 3) {
+                            if (second == (str.charAt(strInd)-'0')) {
+                                pick = dp[strInd+1][first][second][newStrInd+1]%mod;
+                            }
+                        } else if (newStrInd == 4) {
+                            if (first == (str.charAt(strInd)-'0')) {
+                                pick = dp[strInd+1][first][second][newStrInd+1]%mod;
+                            }
+                        } else if (newStrInd == 5) {
+                            pick = 1;
+                        }
+
+                        dp[strInd][first][second][newStrInd] = (pick+nPick)%mod; 
+
+                    }
+                }
+            }
+        }
         // for (int i = 0; i <= s.length(); i++) {
         //     for (int j = 0; j < 10; j++) {
         //         for (int k = 0; k < 10; k++) {
-        //             Arrays.fill(dp[i][j][k], -1);
+        //             System.out.print(Arrays.toString(dp[i][j][k]) +" ");
         //         }
+        //         System.out.print("\t");
         //     }
+        //     System.out.print("\n");
         // }
-        return dfs(0, 9, 9, 0, s);
+        return dp[0][9][9][0];
     }
     private static int mod = 1000_000_007; 
     private int dfs (int strInd, int first, int second, int newStrInd, String str) {
@@ -41,6 +88,6 @@ class Solution {
             }
         }
 
-        return dp[strInd][first][second][newStrInd] = (pick+nPick)%mod;    
+        return dp[strInd][first][second][newStrInd] = (pick+nPick)%mod;  
     }
 }
