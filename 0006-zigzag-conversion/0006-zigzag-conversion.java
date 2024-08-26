401c52f6062178c5e1f0df756fe795752b4012d9
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-26 12:29:11 +0530

    Time: 4 ms (81.27%), Space: 45 MB (64.19%)

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -3,22 +3,21 @@ class Solution {
         StringBuilder[] sb = new StringBuilder[numRows];
         for (int i = 0; i < numRows; i++) {
             sb[i] = new StringBuilder();
-        }   
-        int n = s.length();
-        for (int i = 0; i < n;) {
-            int j = 0;
-            while (j < numRows && i < n) {
-                sb[j++].append(s.charAt(i++));
-            }    
-            j -= 2;
-            while (j > 0 && i < n) {
-                sb[j--].append(s.charAt(i++));
-            }
+        }
 
+        int n = s.length();
+        int idx = 0;
+        while (idx < n) {
+            for (int j = 0; j < numRows && idx < n; j++) {
+                sb[j].append(s.charAt(idx++));
+            }
+            for (int j = numRows - 2; j >= 1 && idx < n; j--) {
+                sb[j].append(s.charAt(idx++));
+            }
         }
 
         for (int i = 1; i < numRows; i++) {
-            sb[0].append(sb[i].toString());
+            sb[0].append(sb[i]);
         }
 
         return sb[0].toString();
