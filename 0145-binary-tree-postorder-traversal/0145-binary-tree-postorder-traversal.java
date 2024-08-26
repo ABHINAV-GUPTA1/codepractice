54ee2b5b1dd2f80905d39ad40ab8cc9faf09c1fc
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-26 12:04:18 +0530

    Time: 142 ms (9.25%), Space: 41.4 MB (82.52%)

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -20,10 +20,58 @@ class Solution {
         if (root == null) {
             return res;
         }
+        // method 1 traversal in recursion
+        // dfs(root);
 
-        dfs(root);
-
+        // method 2 morris traversal
+        morrisTraversal(root);
         return res;
+    }
+
+    private void morrisTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        TreeNode curr = root;
+
+        while (curr != null) {
+            if (curr.right == null) {
+                res.add(curr.val);
+                curr = curr.left;
+            } else {
+                TreeNode prev = getRight(curr.right, curr);
+                if (prev.left == null) {
+                    res.add(curr.val);
+                    prev.left = curr;
+                    curr = curr.right;
+                } else {
+                    prev.left = null;
+                    curr = curr.left;
+                }
+            }
+        }
+        reverse();
+    }
+
+    private void reverse() {
+        int left = 0;
+        int right = res.size()-1;
+        while (left < right) {
+            int t = res.get(left);
+            res.set(left, res.get(right));
+            res.set(right, t);
+            left++;
+            right--;
+        }
+    }
+
+
+    private TreeNode getRight(TreeNode curr, TreeNode root) {
+        while (curr.left != null && curr.left != root) {
+            curr = curr.left;
+        }
+        return curr;
     }
 
     private void dfs(TreeNode root) {
