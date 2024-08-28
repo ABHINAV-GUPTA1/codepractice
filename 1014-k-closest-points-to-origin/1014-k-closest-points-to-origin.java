e386c0f2bb952407dbdfbb91d71a2ee14b6fd0da
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-28 18:43:08 +0530

    Time: 30 ms (59.14%), Space: 53.1 MB (48.33%)

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,27 +1,7 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int[][] res = new int[k][2];
-        /**
-        Method 1 -> using min heap  
-        
-        
-        PriorityQueue<int[]> pq =  new PriorityQueue<>((a,b)-> ((a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1]) ) );
-
-        for (int i = 0; i < points.length; i++) {
-            pq.offer(points[i]);
-        }
-
-        for (int i = 0; i < k; i++) {
-            res[i] = pq.poll();
-        }
-
-        return res;
-        */
-
-        /**
-            Method 2 -> using max heap
-         */
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> ((b[0]*b[0]+b[1]*b[1]) - (a[0]*a[0]+a[1]*a[1])) );
+        List<int[]> res = new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(-(a[0]*a[0]+a[1]*a[1])+(b[0]*b[0]+b[1]*b[1])));
         for (int i = 0; i < points.length; i++) {
             pq.offer(points[i]);
             if (pq.size() > k) {
@@ -29,10 +9,10 @@ class Solution {
             }
         }
 
-        for (int i = 0; i < k; i++) {
-            res[i] = pq.poll();
+        while (!pq.isEmpty()) {
+            res.add(pq.poll());
         }
 
-        return res;
+        return res.toArray(new int[res.size()][]);
     }
 }
