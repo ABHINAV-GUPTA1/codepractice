c0b7080fa2e6d7f156b7a5f85ff8cb65ef37b245
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-28 11:21:26 +0530

    Time: 2 ms (41.3%), Space: 56.9 MB (80.15%)

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        boolean isOne = false;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                isOne = true;
+            }
+            if (nums[i] > n || nums[i] < 1) {
+                nums[i] = 1;
+            }
+        }
+        if (!isOne) {
+            return 1;
+        }
+        for (int i = 0; i < n; i++) {
+            nums[Math.abs(nums[i]) - 1] = - Math.abs(nums[Math.abs(nums[i]) - 1]);
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0) {
+                return i+1;
+            }
+        }
+
+        return n+1;
+    }
+}
