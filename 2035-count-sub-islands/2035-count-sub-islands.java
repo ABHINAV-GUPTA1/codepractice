f7e248719b76a0adb2a8df8b61ca55c0ac81c4e2
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-08-28 11:52:38 +0530

    Time: 24 ms (47.76%), Space: 84.7 MB (43.47%)

--- 2035-count-sub-islands/2035-count-sub-islands.java
+++ 2035-count-sub-islands/2035-count-sub-islands.java
@@ -1,11 +1,11 @@
 class Solution {
+    
     public int countSubIslands(int[][] grid1, int[][] grid2) {
         int count = 0;
-        boolean[][] vis = new boolean[grid2.length][grid2[0].length];
         for (int i = 0; i < grid2.length; i++) {
             for (int j = 0; j < grid2[i].length; j++) {
-                if (grid2[i][j] == 1 && !vis[i][j]) {
-                    if (dfs(i, j, grid1, grid2, vis)) {
+                if (grid2[i][j] == 1 ) {
+                    if (dfs(i, j, grid1, grid2)) {
                         count++;
                     }
                 }
@@ -14,7 +14,40 @@ class Solution {
         return count;
     }
 
-    private boolean dfs(int i, int j, int[][] g1, int[][] g2, boolean[][] vis) {
+    private boolean dfs(int i, int j, int[][] g1, int[][] g2) {
+        if (i < 0 || j < 0 || i >= g2.length || j >= g2[0].length || g2[i][j] == 0 || g2[i][j] == 2) {
+            return true;
+        }
+        g2[i][j] = 2;
+        boolean res = true;
+        if (g1[i][j] == 0) {
+            res = false;
+        }
+
+        res = dfs(i+1, j, g1, g2) && res;
+        res = dfs(i-1, j, g1, g2) && res;
+        res = dfs(i, j+1, g1, g2) && res;
+        res = dfs(i, j-1, g1, g2) && res;
+
+        return res;
+    }
+    
+    public int countSubIslands1(int[][] grid1, int[][] grid2) {
+        int count = 0;
+        boolean[][] vis = new boolean[grid2.length][grid2[0].length];
+        for (int i = 0; i < grid2.length; i++) {
+            for (int j = 0; j < grid2[i].length; j++) {
+                if (grid2[i][j] == 1 && !vis[i][j]) {
+                    if (dfs1(i, j, grid1, grid2, vis)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    private boolean dfs1(int i, int j, int[][] g1, int[][] g2, boolean[][] vis) {
         if (i < 0 || j < 0 || i >= g2.length || j >= g2[0].length || g2[i][j] == 0 || vis[i][j]) {
             return true;
         }
@@ -24,10 +57,10 @@ class Solution {
             res = false;
         }
 
-        res = dfs(i+1, j, g1, g2, vis) && res;
-        res = dfs(i-1, j, g1, g2, vis) && res;
-        res = dfs(i, j+1, g1, g2, vis) && res;
-        res = dfs(i, j-1, g1, g2, vis) && res;
+        res = dfs1(i+1, j, g1, g2, vis) && res;
+        res = dfs1(i-1, j, g1, g2, vis) && res;
+        res = dfs1(i, j+1, g1, g2, vis) && res;
+        res = dfs1(i, j-1, g1, g2, vis) && res;
 
         return res;
     }
