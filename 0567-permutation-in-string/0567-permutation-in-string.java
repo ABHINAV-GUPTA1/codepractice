c3780a7e7bf76f2fdee78485539cc68909c3968c
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-10-05 11:27:38 +0530

    Time: 5 ms (96.14%), Space: 42.7 MB (53.57%)

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,18 +2,14 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int l1 = s1.length();
         int l2 = s2.length();
-        if (l1 > l2) {
-            return false;
-        }
+        int i = 0, j = 0;
         int map[] = new int[26];
-        for (int i = 0; i < l1; i++) {
+        for (; i < l1; i++) {
             map[s1.charAt(i)-'a']++;
         }
-
-        int i = 0;
-        int j = 0;
+        i = 0;
         while (i < l2) {
-            map[s2.charAt(i)-'a']--;
+            map[s2.charAt(i) - 'a']--;
             if (i-j+1 > l1) {
                 map[s2.charAt(j++)-'a']++;
             }
@@ -27,7 +23,7 @@ class Solution {
     }
 
     private boolean check(int[] map) {
-        for (int i = 0; i < 26; i++) {
+        for (int i = 0; i <26; i++) {
             if (map[i] != 0) {
                 return false;
             }
