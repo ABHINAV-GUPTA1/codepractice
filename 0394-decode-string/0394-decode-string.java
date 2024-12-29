b9d1db1bce921dee5e79f8da508c71344607cbda
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2024-12-29 18:14:52 +0530

    Time: 1 ms (70.38%), Space: 41.5 MB (68.92%)

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,34 +1,42 @@
 class Solution {
     public String decodeString(String s) {
+        
         Stack<Integer> stCount = new Stack<>();
         Stack<StringBuilder> stStr = new Stack<>();
-        int idx = 0;
+        
+        StringBuilder res = new StringBuilder("");
+        StringBuilder tmp;
+
         int n = s.length();
-        StringBuilder res = new StringBuilder();
+        int idx = 0;
+        int val;
+        
         while (idx < n) {
             if (Character.isDigit(s.charAt(idx))) {
-                int count = 0;
+                val = 0;
                 while (idx < n && Character.isDigit(s.charAt(idx))) {
-                    count = count*10 + (s.charAt(idx) - '0');
+                    val = val * 10 + (s.charAt(idx) - '0');
                     idx++;
                 }
-                stCount.push(count);
+                stCount.push(val);
+                continue;
             } else if (s.charAt(idx) == '[') {
                 stStr.push(res);
                 res = new StringBuilder();
-                idx++;
             } else if (s.charAt(idx) == ']') {
-                StringBuilder tmp = stStr.pop();
-                int count = stCount.pop();
-                for (int i = 0; i < count; i++) {
+                tmp = stStr.pop();
+                val = 0;
+                while (val < stCount.peek()) {
                     tmp.append(res);
+                    val++;
                 }
+                stCount.pop();
                 res = tmp;
-                idx++;
             } else {
-                res.append(s.charAt(idx++));
+                res.append(s.charAt(idx));
             }
+            idx++;
         }
-        return res.toString();
+        return res.toString();    
     }
 }
