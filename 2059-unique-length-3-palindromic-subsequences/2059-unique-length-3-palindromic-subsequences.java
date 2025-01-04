8d66e4ce80169a4160f54937f60de05584f2951e
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-01-04 21:05:35 +0530

    Time: 204 ms (51.05%), Space: 45.6 MB (39.53%)

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,30 +1,33 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        List<int[]> list = new ArrayList<>(Arrays.asList(new int[26][]));
-        int ans = 0;
+        int[][] charPositions = new int[26][2];
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(charPositions[i], -1); // Initialize efficiently
+        }
+
         for (int i = 0; i < s.length(); i++) {
             int charIdx = s.charAt(i) - 'a';
-            if (list.get(charIdx) == null) {
-                list.set(charIdx, new int[]{-1,-1});
+            if (charPositions[charIdx][0] == -1) {
+                charPositions[charIdx][0] = i;
             }
-            if (list.get(charIdx)[0] == -1) {
-                list.get(charIdx)[0] = i;
-            }
-            list.get(charIdx)[1] = i;
-            // System.out.println("\t char"+charIdx);
+            charPositions[charIdx][1] = i;
         }
-        Set<Character> set;
+
+        int ans = 0;
         for (int i = 0; i < 26; i++) {
-            if (list.get(i) == null) {
+            if (charPositions[i][0] == -1) {
                 continue;
             }
-            int leftIdx = list.get(i)[0];
-            int rightIdx = list.get(i)[1];
-            set = new HashSet<>();
-            for (int j = leftIdx+1; j < rightIdx; j++) {
-                set.add(s.charAt(j));
+            int leftIdx = charPositions[i][0];
+            int rightIdx = charPositions[i][1];
+
+            if (rightIdx - leftIdx > 1) { //Optimization: skip if no inner characters
+                Set<Character> set = new HashSet<>();
+                for (int j = leftIdx + 1; j < rightIdx; j++) {
+                    set.add(s.charAt(j));
+                }
+                ans += set.size();
             }
-            ans += set.size();
         }
 
         return ans;
