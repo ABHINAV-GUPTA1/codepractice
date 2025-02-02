0136d908165d8a6c94153303142b3f41af0cf6e3
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-02-02 17:24:45 +0530

    Time: 9 ms (50.52%), Space: 42.7 MB (80.15%)

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,17 +1,21 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] map = new int[26];
+        int k = 0;
         for (int i = 0; i < s1.length(); i++) {
             map[s1.charAt(i)-'a']++;
+            k += (s1.charAt(i)-'a');
         }
         int head = 0;
         int tail = 0;
         while (tail < s2.length()) {
             map[s2.charAt(tail)-'a']--;
+            k -= (s2.charAt(tail)-'a');
             if (tail-head+1 > s1.length()) {
                 map[s2.charAt(head++)-'a']++;
+                k += (s2.charAt(head-1)-'a');
             }
-            if (checkMap(map)) {
+            if (k == 0 && checkMap(map)) {
                 return true;
             }
             tail++;
