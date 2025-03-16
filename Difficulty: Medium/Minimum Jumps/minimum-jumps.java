8704298b25ae16d27dde9a3b99bce945096f337c
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-03-16 22:32:47 +0530

    Added Solution

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            System.out.println(new Solution().minJumps(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static int minJumps(int[] arr) {
+        int ans = 0;
+        int currMax = 0;
+        int curr = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            currMax = Math.max(currMax, arr[i] + i);
+            if (i == curr) {
+                curr = currMax;
+                ans++;
+            }
+        }
+        
+        return curr < arr.length-1 ? -1 : ans;
+    }
+}
