5be3e14030f9d7e6aa3667ab8fb194056dd7f2a9
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-03-17 19:26:14 +0530

    Time: 1 ms (99.51%), Space: 56 MB (99.71%)

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+        @author: Abhinav Gupta 
+     */
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
