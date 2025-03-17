0467a0cd2f241d71abf12a7b0011b17cdbe74efa
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-03-17 17:36:28 +0530

    Time: 0 ms (100%), Space: 45.4 MB (42.48%)

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // int ans = 0;
-        // for (int i = 0; i < nums.length; i++) {
-        //     ans ^= i;
-        //     ans ^= nums[i];
-        // }    
-        // ans ^= nums.length;
-
-        // return ans;
-
-        int n = nums.length;
-        int sum = n * (n+1) / 2;
-        for (int i = 0; i < n; i++) {
-            sum -= nums[i];
-        }
-
-        return sum;
-
+        int ans = nums[0] ^ 0;
+        for (int i = 1; i < nums.length; i++) {
+            ans ^= nums[i];
+            ans ^= i;
+        }    
+        return ans^nums.length;
     }
 }
