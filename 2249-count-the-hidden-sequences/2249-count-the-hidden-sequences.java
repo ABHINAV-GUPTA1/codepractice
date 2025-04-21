a09dd42ea4607f3ff28c209d27e89967c740749c
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-04-21 15:53:06 +0530

    Time: 4 ms (92.06%), Space: 61.6 MB (49.21%)

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.java
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.java
@@ -1,19 +1,19 @@
 class Solution {
     public int numberOfArrays(int[] differences, int lower, int upper) {
-        int curr = 0;
-        int minVal = 0;
-        int maxVal = 0;
+        long curr = 0;
+        long minVal = 0;
+        long maxVal = 0;
         int n = differences.length;
-        int diff = 0;
+        long diff;
+        
         for (int i = 0; i < n; i++) {
             curr = curr + differences[i];
             minVal = Math.min(curr, minVal);
             maxVal = Math.max(curr, maxVal);
-            diff = (upper - maxVal) - (lower - minVal) + 1;
-            if (diff <= 0) {
-                return 0;
-            }
-        }    
-        return diff;
+        }
+        
+        diff = (upper - maxVal) - (lower - minVal) + 1;
+        
+        return diff <= 0 ? 0 : (int) diff;
     }
 }
