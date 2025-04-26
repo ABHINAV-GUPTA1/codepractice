af5979599ebc839107d7adbbb5e58a9afce944a5
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-04-27 00:05:58 +0530

    Time: 7 ms (95.85%), Space: 60.7 MB (32.34%)

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.java
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.java
@@ -1,22 +1,23 @@
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
         long ans = 0;
-        int mini = -1;
-        int maxi = -1;
-        int ci = -1;
-        int temp = -1;
+        int minIdx = -1;
+        int maxIdx = -1;
+        int culpritIdx = -1;
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < minK || nums[i] > maxK) {
-                ci = i;
-            }
             if (nums[i] == minK) {
-                mini = i;
+                minIdx = i;
             }
             if (nums[i] == maxK) {
-                maxi = i;
+                maxIdx = i;
             }
-            temp = (Math.min(mini, maxi)) - ci;
-            ans += (temp<=0 ? 0 : temp); 
+            if (nums[i] < minK || nums[i] > maxK) {
+                culpritIdx = i;
+            }
+            int smaller = Math.min(minIdx, maxIdx);
+            int tempVal = smaller - culpritIdx;
+            ans += tempVal > 0 ? tempVal : 0;
         }
         return ans;
     }
