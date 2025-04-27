59ebc1430d74dbc17bdc1b36d54a699dabf245cc
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-04-27 22:22:44 +0530

    Time: 0 ms (100%), Space: 42.3 MB (41.26%)

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,7 +1,13 @@
 class Solution {
+    /**
+        2 step process
+        1. Transpose
+        2. Reverse
+     */
     public void rotate(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
+        // Transpose
         for (int i = 0; i < m; i++) {
             for (int j = i; j < n; j++) {
                 int t = matrix[i][j];
@@ -9,12 +15,22 @@ class Solution {
                 matrix[j][i] = t;
             }
         }
+
+        //reverse the matrix
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n/2; j++) {
-                int t = matrix[i][j];
-                matrix[i][j] = matrix[i][n-j-1];
-                matrix[i][n-j-1] = t;
-            }
-        }    
+            reverse(matrix[i]);
+        }
+    }
+
+    private void reverse(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int t = arr[left];
+            arr[left] = arr[right];
+            arr[right] = t;
+            left++;
+            right--;
+        }
     }
 }
