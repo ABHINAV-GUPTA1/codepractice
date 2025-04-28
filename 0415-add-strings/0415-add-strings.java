1f9de6f5a50666ac2005b9aa17bab5f584e39b33
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-04-28 18:11:51 +0530

    Time: 1 ms (100%), Space: 42.5 MB (85.78%)

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,23 +1,20 @@
 class Solution {
     public String addStrings(String num1, String num2) {
+        
         StringBuilder sb = new StringBuilder();
-        int i = num1.length()-1;
-        int j = num2.length()-1;
+        
+        int idx1 = num1.length() - 1;
+        int idx2 = num2.length() - 1;
         int carry = 0;
-        int v1, v2;
-        while (i >= 0 || j >= 0) {
-            v1 = i >= 0 ? num1.charAt(i) - '0' : 0;
-            v2 = j >= 0 ? num2.charAt(j) - '0' : 0;
-            int sum = v1+v2+carry;
-            carry = sum / 10;
-            sb.append((sum%10));
 
-            i--;
-            j--;
-        }    
-        if (carry > 0) {
-            sb.append(carry);
+        while (idx1 >= 0 || idx2 >= 0 || carry > 0) {
+            int val1 = idx1 >= 0 ? num1.charAt(idx1--) - '0' : 0;
+            int val2 = idx2 >= 0 ? num2.charAt(idx2--) - '0' : 0;
+            int sum = val1 + val2 + carry;
+            sb.append(sum % 10);
+            carry = sum / 10;
         }
+
         return sb.reverse().toString();
     }
 }
