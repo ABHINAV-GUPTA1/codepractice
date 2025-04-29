48f220d9870bdda2f728ec58b507333519ba39f5
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-04-29 21:36:51 +0530

    Time: 1 ms (64.16%), Space: 41.5 MB (77%)

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,12 +1,17 @@
 class Solution {
+    
+    private static final String EMPTY = "";
+    
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
+        
+        if (strs == null || strs[0].length() == 0) {
+            return EMPTY;
         }
-
+        
         int len = strs[0].length();
+        int arrLen = strs.length;
 
-        for (int i = 1; i < strs.length; i++) {
+        for (int i = 1; i < arrLen; i++) {
             len = Math.min(len, strs[i].length());
             for (int j = 0; j < len; j++) {
                 if (strs[0].charAt(j) != strs[i].charAt(j)) {
