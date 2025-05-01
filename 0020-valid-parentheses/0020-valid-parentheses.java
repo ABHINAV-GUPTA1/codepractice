9e18e6012244f617ec30a43d0b3dc197a4647d86
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-01 12:50:08 +0530

    Time: 3 ms (41.74%), Space: 41.8 MB (48.13%)

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
+        
         Stack<Character> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+        int length = s.length();
+        
+        for (int i = 0; i < length; i++) {
+            if (s.charAt(i) == '(' 
+                || s.charAt(i) == '{' 
+                || s.charAt(i) == '[') {
                 st.push(s.charAt(i));
             } else {
-                if (st.isEmpty() || !(isValid(st.peek(), s.charAt(i)))) {
+                if (st.isEmpty() 
+                    || !(isValid(st.peek(), s.charAt(i)))) {
                     return false;
-                }
+                } 
                 st.pop();
             }
-        }
+        }    
+        
         return st.isEmpty();
     }
 
     private boolean isValid(char a, char b) {
-        return (a == '(' && b == ')') || (a == '{' && b == '}') || (a == '[' && b == ']');
+        return (a == '(' && b == ')')
+                || (a == '{' && b == '}')
+                || (a == '[' && b == ']');
     }
 }
