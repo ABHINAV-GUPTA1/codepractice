0269f24f11bb3b3bfed852816c60e514f73463e8
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-02 13:06:01 +0530

    Time: 12 ms (33.26%), Space: 47.3 MB (58.21%)

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -2,14 +2,14 @@ class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int low = 0;
         int high = 0;
-        for (int i : weights) {
-            low = Math.max(low, i);
-            high += i;
+        for (int i = 0; i < weights.length; i++) {
+            high += weights[i];
+            low = Math.max(low, weights[i]);
         }
-        int ans = days;
+        int ans = 0;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (isPossible(mid, weights, days)) {
+            if (isPossible(weights, mid, days)) {
                 ans = mid;
                 high = mid - 1;
             } else {
@@ -20,17 +20,19 @@ class Solution {
         return ans;
     }
 
-    boolean isPossible(int val, int[] arr, int days) {
-        int sum = arr[0];
+    private boolean isPossible(int[] arr, int mid, int days) {
+        int sum = 0;
         int count = 1;
-        for (int i = 1; i < arr.length; i++) {
-            if (sum + arr[i] <= val) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (sum + arr[i] <= mid) {
                 sum += arr[i];
             } else {
                 count++;
                 sum = arr[i];
             }
         }
+
         return count <= days;
     }
 }
