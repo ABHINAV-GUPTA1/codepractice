3e0fc08bed52dba1ae99556c9e37e7e44c6eb582
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-03 14:12:24 +0530

    Time: 7 ms (65.42%), Space: 47.8 MB (82.69%)

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,12 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        for (int i = 0; i < strs.length; i++) {
-            char c[] = strs[i].toCharArray();
-            Arrays.sort(c);
-            map.computeIfAbsent(new String(c), x->new ArrayList<>()).add(strs[i]);
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            map.computeIfAbsent(String.valueOf(arr), x->new ArrayList<>()).add(str);
         }
 
-        return map.values().stream().collect(Collectors.toList());
+        List<List<String>> res = new ArrayList<>();
+        for (Map.Entry<String, List<String>> me : map.entrySet()) {
+            res.add(me.getValue());
+        }
+        return res;
     }
 }
