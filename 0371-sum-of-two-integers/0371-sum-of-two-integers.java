acf538e453c63a781dbc5130e9eae67cefa89de6
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-03 22:24:08 +0530

    Time: 0 ms (100%), Space: 40.2 MB (79.15%)

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,9 +1,8 @@
 class Solution {
     public int getSum(int a, int b) {
-        int tmp;
         while (b != 0) {
-            tmp = (a&b) << 1;
-            a = a^b;
+            int tmp = (a&b) << 1;
+            a = (a ^ b);
             b = tmp;
         }
 
