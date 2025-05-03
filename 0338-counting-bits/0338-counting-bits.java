1eadc78c03bb56141f77db38cd73147bb8665421
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-03 21:53:12 +0530

    Time: 2 ms (94.7%), Space: 47 MB (12.64%)

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n+1];
-        for (int i = 1; i <= n; i++) {
+        n++;
+        int[] ans = new int[n];
+        for (int i = 1; i < n; i++) {
             ans[i] = (i&1) + ans[i/2];
         }
         return ans;
