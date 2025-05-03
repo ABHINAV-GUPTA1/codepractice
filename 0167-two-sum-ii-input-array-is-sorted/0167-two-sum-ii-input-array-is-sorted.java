07414a4f4c47f862c65aa47d58c2328b6d761df0
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-03 22:52:28 +0530

    Time: 2 ms (92.99%), Space: 47.4 MB (37.33%)

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        
         int left = 0;
         int right = numbers.length - 1;
-        int k = 0;
         while (left < right) {
-            k = numbers[left] + numbers[right];
-            if (k == target) {
-                return new int[]{left+1, right+1};
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
             }
-            if (k > target) {
-                right--;
-            } else {
+            if (sum < target) {
                 left++;
+            } else {
+                right--;
             }
         }
-        return new int[] {-1,-1};
+
+        return new int[0];
     }
 }
