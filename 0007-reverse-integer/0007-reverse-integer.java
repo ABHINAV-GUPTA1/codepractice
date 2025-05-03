e952b4eb6d9c433411fe14eab4793b2278f2be59
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-03 22:40:07 +0530

    Time: 1 ms (83.68%), Space: 41.6 MB (6.25%)

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,27 +1,16 @@
 class Solution {
     public int reverse(int x) {
-        // long res = 0;
-        // while (x != 0) {
-        //     res = res*10 + x%10;
-        //     x = x/10;
-        // }
-        // if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
-        //     return 0;
-        // }
-
-        // return (int)res;
-
-        int res = 0;
         int tmp;
+        int res = 0;
         while (x != 0) {
-            tmp = res *10 + x%10;
+            tmp = (res * 10) + (x % 10);
             if (tmp/10 != res) {
                 return 0;
             }
             res = tmp;
-            x = x/10;
+            x = x / 10;
         }
-
         return res;
     }
+
 }
