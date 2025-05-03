50356042b6996a9ad3121e35d5fcad774233aaa4
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-03 14:08:20 +0530

    Time: 123 ms (52.4%), Space: 61.6 MB (69.62%)

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -1,25 +1,17 @@
 class Solution {
-    /***
-        abs(i - j) <= indexDiff
-        arr[i] <= valueDiff + nums[j]
-     */
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
-        
-        TreeSet<Long> set = new TreeSet<>();
-
+        TreeSet<Integer> set = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
-            long num = (long) nums[i];
-            Long ceilingNumber = set.ceiling(num-valueDiff);
-            if (ceilingNumber != null && Math.abs(ceilingNumber - num) <= valueDiff) {
+            int num = nums[i];
+            Integer ceiling = set.ceiling(num-valueDiff);
+            if (ceiling != null && Math.abs(ceiling-num) <= valueDiff) {
                 return true;
             }
-            
             set.add(num);
             if (i >= indexDiff) {
-                set.remove((long) nums[i-indexDiff]);
+                set.remove(nums[i-indexDiff]);
             }
-        } 
-
+        }
         return false;
     }
 }
