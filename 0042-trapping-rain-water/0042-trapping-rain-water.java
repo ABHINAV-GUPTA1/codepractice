e3930408e86a094a6705835fc6fdff356c51df11
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-04 11:30:26 +0530

    Time: 0 ms (100%), Space: 46.6 MB (39.41%)

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,53 +1,23 @@
 class Solution {
-    
-    public int trap(int[] height) { 
-        int ans = 0;
-        int left = 0;
-        int right = height.length-1;
-        int rightmax = 0;
-        int leftmax = 0;
-
-        while (left < right) {
-            if (height[left] <= height[right]) {
-                if (height[left] > leftmax) {
-                    leftmax = height[left];
-                } else {
-                    ans += leftmax - height[left];
-                }
-                left++;
-            } else {
-                if (height[right] > rightmax) {
-                    rightmax = height[right];
-                } else {
-                    ans += rightmax - height[right];
-                }
-                right--;
-            }
-        }
-
-        return ans;
-
-    }
-    
-    public int trap1(int[] height) {
+    public int trap(int[] height) {
         int ans = 0;
         int left = 0;
         int right = height.length - 1;
-        int leftmax = 0;
-        int rightmax = 0;
+        int leftMax = 0;
+        int rightMax = 0;
         while (left < right) {
-            if (height[left] < height[right]) {
-                if (leftmax < height[left]) {
-                    leftmax = height[left];
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
                 } else {
-                    ans+=leftmax-height[left];
+                    ans += leftMax - height[left];
                 }
                 left++;
             } else {
-                if (rightmax < height[right]) {
-                    rightmax = height[right];
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
                 } else {
-                    ans+=rightmax-height[right];
+                    ans += rightMax - height[right];
                 }
                 right--;
             }
