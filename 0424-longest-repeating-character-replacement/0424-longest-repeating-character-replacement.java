da40a04345be4df921ea72d4d1af85c506d6bfe1
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-04 17:19:19 +0530

    Time: 12 ms (56.44%), Space: 45.2 MB (34.68%)

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,65 +1,21 @@
 class Solution {
-    /**
-    
-        Method 1
-    */
-    public int characterReplacement1(String s, int k) {
-        int head = 0;
-        int tail = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        int ans = 0;
-        int n = s.length();
-        char c;
-        int maxf = 0, cnt;
-        while (tail < n) {
-            c = s.charAt(tail);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-            maxf = Math.max(map.get(c), maxf);
-            while ((tail-head+1)-maxf > k) {
-                c = s.charAt(head++);
-                cnt = map.get(c);
-                if (cnt == 1) {
-                    map.remove(c);
-                } else {
-                    map.put(c, cnt-1);
-                }
-                maxf = 0;
-                for (c = 'A'; c <= 'Z'; c++) {
-                    maxf = Math.max(maxf, map.getOrDefault(c, 0));
-                }
-            }
-            ans = Math.max(ans, tail - head + 1);
-            tail++;
-        }
-
-        return ans;
-    }
-
     public int characterReplacement(String s, int k) {
-        int head = 0;
-        int tail = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        int ans = 0;
-        int n = s.length();
-        char c;
-        int maxf = 0, cnt;
-        while (tail < n) {
-            c = s.charAt(tail);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-            maxf = Math.max(map.get(c), maxf);
-            if ((tail-head+1)-maxf > k) {
-                c = s.charAt(head++);
-                cnt = map.get(c);
-                if (cnt == 1) {
-                    map.remove(c);
-                } else {
-                    map.put(c, cnt-1);
-                }
+        int left = 0, right = 0;
+        int length = s.length();
+        int maxLen = 0;
+        int maxFreq = 0;
+        int[] hash = new int[26];
+        while (right < length) {
+            hash[s.charAt(right) - 'A']++;
+            maxFreq = Math.max(maxFreq, hash[s.charAt(right) - 'A']);
+            if ((right - left + 1) - maxFreq > k) {
+                hash[s.charAt(left++) - 'A']--;
             }
-            ans = Math.max(ans, tail - head + 1);
-            tail++;
+            if ((right-left+1) - maxFreq <= k) {
+                maxLen = Math.max(right - left + 1, maxLen);
+            }
+            right++;
         }
-
-        return ans;
+        return maxLen;
     }
 }
