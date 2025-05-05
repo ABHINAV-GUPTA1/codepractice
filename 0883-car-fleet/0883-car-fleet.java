4e288ce6b0d95a9c7cb3b7349d9e7c04c83c554d
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-05 19:10:31 +0530

    Time: 90 ms (35.85%), Space: 56.2 MB (81.61%)

--- 0883-car-fleet/0883-car-fleet.java
+++ 0883-car-fleet/0883-car-fleet.java
@@ -1,5 +1,31 @@
 class Solution {
+
     public int carFleet(int target, int[] position, int[] speed) {
+        Stack<Double> st = new Stack<>();
+        int len = position.length;
+        double[][] carData = new double[len][2];
+        
+        // carData[i][0] => pos
+        // carData[i][1] => time
+        for (int i = 0; i < len; i++) {
+            carData[i][0] = (double) position[i];
+            carData[i][1] = ((double) target - position[i]) / speed[i];
+        }
+
+        Arrays.sort(carData, (a, b) -> Double.compare(a[0], b[0]));
+        double prev = -1;
+        for (int i = len - 1; i >= 0; i--) {
+            
+            if (!st.isEmpty() && carData[i][1] <= st.peek()) {
+                continue;
+            }
+            st.push(carData[i][1]);
+        }
+
+        return st.size();
+    }
+
+    public int carFleet_method1(int target, int[] position, int[] speed) {
         
         int len = position.length;
         double[][] carData = new double[len][2];
