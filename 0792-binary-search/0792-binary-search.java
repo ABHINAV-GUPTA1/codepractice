99abd57b35639e0c471820c63a18beeee94f32bc
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-05 22:37:05 +0530

    Time: 0 ms (100%), Space: 46.1 MB (22.9%)

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -3,16 +3,17 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
-            int mid = left + (right-left) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
+            if (nums[mid] > target) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
-        }    
+        }
+
         return -1;
     }
 }
