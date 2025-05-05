077ab3523d9a4155b4e81528a8d7e0d653bf8912
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-05 19:25:32 +0530

    Time: 93 ms (29.48%), Space: 56.5 MB (71.68%)

--- 0883-car-fleet/0883-car-fleet.java
+++ 0883-car-fleet/0883-car-fleet.java
@@ -1,6 +1,31 @@
 class Solution {
 
     public int carFleet(int target, int[] position, int[] speed) {
+        Stack<Integer> st = new Stack<>();
+        int len = position.length;
+        double[][] carData = new double[len][2];
+        
+        // carData[i][0] => pos
+        // carData[i][1] => time
+        for (int i = 0; i < len; i++) {
+            carData[i][0] = (double) position[i];
+            carData[i][1] = ((double) target - position[i]) / speed[i];
+        }
+
+        Arrays.sort(carData, (a, b) -> Double.compare(a[0], b[0]));
+
+        for (int i = len - 1; i >= 0; i--) {
+            
+            if (!st.isEmpty() && carData[i][1] <= carData[st.peek()][1]) {
+                continue;
+            }
+            st.push(i);
+        }
+
+        return st.size();
+    }
+
+    public int carFleet_method2(int target, int[] position, int[] speed) {
         Stack<Double> st = new Stack<>();
         int len = position.length;
         double[][] carData = new double[len][2];
@@ -13,7 +38,7 @@ class Solution {
         }
 
         Arrays.sort(carData, (a, b) -> Double.compare(a[0], b[0]));
-        double prev = -1;
+        
         for (int i = len - 1; i >= 0; i--) {
             
             if (!st.isEmpty() && carData[i][1] <= st.peek()) {
