f86f91971f88ad7392aec65cb246b7001b40e549
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-05 12:49:48 +0530

    Time: 4 ms (97.34%), Space: 45 MB (44.04%)

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,6 +1,60 @@
+
 class MinStack {
-    Stack<Pair<Integer, Integer>> st;
+    Stack<Long> st;
+    long min;
     public MinStack() {
+        st = new Stack<>();
+        min = -1;
+    }
+    
+    public void push(int val) {
+        if (st.isEmpty()) {
+            min = val;
+            st.push((long) val);
+        } else {
+            if (min < val) {
+                st.push((long) val);
+            } else {
+                long newVal = (long) 2*val - min;
+                st.push(newVal);
+                min = val;
+            }
+        }
+    }
+    
+    public void pop() {
+        if (st.isEmpty()) {
+            return;
+        }
+        long val = st.pop();
+        if (val < min) {
+            min = 2 * min - val;
+        }
+    }
+    
+    public int top() {
+        if (st.isEmpty()) {
+            return -1;
+        }
+        long val = st.peek();
+        return (int) ((val > min) ? val : min);
+    }
+    
+    public int getMin() {
+        if (st.isEmpty()) {
+            return -1;
+        }
+        return (int) min;
+    }
+}
+
+/***
+    Using Pair in stack but not space efficient
+    TC: O(1) for getMin
+ */
+class MinStackMethod1 {
+    Stack<Pair<Integer, Integer>> st;
+    public MinStackMethod1() {
         st = new Stack<>();
     }
     
