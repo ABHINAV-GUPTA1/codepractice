9f5afc55141b6af53589cddc45fdd801ad023380
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-05 15:52:30 +0530

    Time: 1 ms (81.89%), Space: 43.5 MB (26.27%)

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        generate(0, 0, n, "", res);
+        int open = 0;
+        int close = 0;
+        generate("", n, open, close, res);
         return res;
     }
 
-    private void generate(int open, int close, int n, String s, List<String> res) {
-        if (s.length() == n*2) {
-            res.add(s);
+    private void generate(String str, int n, int open, int close, List<String> res) {
+    
+        if (str.length() == 2*n) {
+            res.add(str);
             return;
         }
-        StringBuilder sb = new StringBuilder(s);
         if (open < n) {
-            sb.append("(");
-            generate(open+1, close, n, sb.toString(), res);
-            sb.setLength(sb.length()-1);
+            generate(str+"(", n, open + 1, close, res);
         }
         if (close < open) {
-            sb.append(")");
-            generate(open, close+1, n, sb.toString(), res);
+            generate(str+")", n, open, close + 1, res);
         }
     }
 }
