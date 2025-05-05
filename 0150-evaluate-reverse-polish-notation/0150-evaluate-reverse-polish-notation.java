cee4a3260b6de2b9a8e58f5a0b5d4cbb2b17161c
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-05 14:34:24 +0530

    Time: 8 ms (12.43%), Space: 44.3 MB (96.46%)

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,5 +1,40 @@
 class Solution {
+
+    /**
+        Using BiFunction and Map
+     */
     public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        Map<Character, BiFunction<Integer, Integer, Integer>> map = initializeBiFunctionMap();
+        for (String token : tokens) {
+            char c = token.charAt(token.length()-1);
+            if (Character.isDigit(c)) {
+                st.push(Integer.parseInt(token));
+            } else {
+                int b = st.pop();
+                int a = st.pop();
+                st.push(map.get(c).apply(a, b));
+            }
+        }
+
+        return st.pop();
+    }
+
+    private Map<Character, BiFunction<Integer, Integer, Integer>> initializeBiFunctionMap() {
+        
+        Map<Character, BiFunction<Integer, Integer, Integer>> map = new HashMap<>();
+        map.put('+', (a,b)->a+b);
+        map.put('-', (a,b)->a-b);
+        map.put('*', (a,b)->(int)((long)a*b));
+        map.put('/', (a,b)->a/b);
+        
+        return map;
+    }
+
+    /**
+        Basic Approach
+     */
+    public int evalRPN_method1(String[] tokens) {
         Stack<Integer> st = new Stack<>();
         for (String token : tokens) {
             char c = token.charAt(token.length()-1);
