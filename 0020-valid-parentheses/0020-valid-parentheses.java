800717d16eb5bdb77fee942d5114df3c951074c3
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-05 11:22:25 +0530

    Time: 2 ms (97.32%), Space: 41.8 MB (65.93%)

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,27 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        
         Stack<Character> st = new Stack<>();
         int length = s.length();
-        
         for (int i = 0; i < length; i++) {
-            if (s.charAt(i) == '(' 
-                || s.charAt(i) == '{' 
-                || s.charAt(i) == '[') {
+            if (isValidForInsert(s.charAt(i))) {
                 st.push(s.charAt(i));
             } else {
-                if (st.isEmpty() 
-                    || !(isValid(st.peek(), s.charAt(i)))) {
+                if (st.isEmpty() || !isValidCombination(st.peek(), s.charAt(i))) {
                     return false;
                 } 
                 st.pop();
             }
-        }    
-        
+        }
         return st.isEmpty();
     }
 
-    private boolean isValid(char a, char b) {
+    private boolean isValidForInsert(char a) {
+        return a == '(' || a == '[' || a == '{';
+    }
+
+    private boolean isValidCombination(char a, char b) {
         return (a == '(' && b == ')')
                 || (a == '{' && b == '}')
                 || (a == '[' && b == ']');
