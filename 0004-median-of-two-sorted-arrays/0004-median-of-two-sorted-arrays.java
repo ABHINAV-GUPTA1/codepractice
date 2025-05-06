5e7f7160a269598cb2377bbdeacc00cfdb6ab6c2
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-06 12:52:43 +0530

    Time: 1 ms (100%), Space: 46.5 MB (11.76%)

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,24 +2,22 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-
         if (n1 > n2) {
             return findMedianSortedArrays(nums2, nums1);
-        }
-
+        }    
+        int n = n1 + n2;
         int left = (n1 + n2 + 1) / 2;
         int low = 0;
         int high = n1;
-        int n = n1 + n2;
         while (low <= high) {
-            
-            int l1 = Integer.MIN_VALUE;
-            int l2 = Integer.MIN_VALUE;
-            int r1 = Integer.MAX_VALUE;
-            int r2 = Integer.MAX_VALUE;
-
             int mid1 = low + (high - low) / 2;
             int mid2 = left - mid1;
+
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
 
             if (mid1 < n1) {
                 r1 = nums1[mid1];
@@ -27,7 +25,7 @@ class Solution {
             if (mid2 < n2) {
                 r2 = nums2[mid2];
             }
-            
+
             if (mid1 - 1 >= 0) {
                 l1 = nums1[mid1 - 1];
             }
@@ -37,7 +35,7 @@ class Solution {
 
             if ((l1 <= r2) && (l2 <= r1)) {
                 if (n%2 == 0) {
-                    return ((double) Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
                 } else {
                     return Math.max(l1, l2);
                 }
@@ -47,7 +45,9 @@ class Solution {
             } else {
                 low = mid1 + 1;
             }
+
         }
-        return 0;
+
+        return -1;
     }
 }
