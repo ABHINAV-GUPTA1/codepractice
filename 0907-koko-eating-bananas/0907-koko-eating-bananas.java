c9aeb96150d7c412f0ea39a933c2bd59c1358f19
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-06 11:47:20 +0530

    Time: 25 ms (42.9%), Space: 45.3 MB (35.86%)

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,30 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int low = 1;
-        Arrays.sort(piles);
         int high = 1;
-        int ans = 1;
         for (int i = 0; i < piles.length; i++) {
             high = Math.max(high, piles[i]);
         }
-
+        int ans = 0;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (isValid(piles, mid, h)) {
+            if (isPossible(mid, piles, h)) {
                 ans = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
+
         return ans;
     }
 
-    private boolean isValid(int[] arr, int mid, int h) {
+    private boolean isPossible(int mid, int[] arr, int h) {
         int count = 0;
-        double t = mid;
+        double tmp = mid;
         for (int i = 0; i < arr.length; i++) {
-            count += Math.ceil(arr[i] / t);
+            count += Math.ceil(arr[i] / tmp);
         }
         return count <= h;
     }
