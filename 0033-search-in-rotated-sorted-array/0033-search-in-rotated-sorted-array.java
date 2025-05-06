11effd3eb5a7d403abfd11de02a50f536c843372
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-06 12:16:35 +0530

    Time: 0 ms (100%), Space: 42 MB (68.16%)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,11 +1,9 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
         int left = 0;
         int right = nums.length - 1;
-        
+
         while (left <= right) {
-            
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
@@ -23,7 +21,6 @@ class Solution {
                     right = mid - 1;
                 }
             }
-
         } 
 
         return -1;
