1bcfedcd12a9963cd47e962f1d5ae9d2887f9ca2
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-08 12:44:39 +0530

    Time: 5 ms (100%), Space: 45.2 MB (39.01%)

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,7 +9,28 @@
  */
 
 class Solution {
+    /**
+        Method 2 using BST property
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        if ((root.val > p.val && root.val < q.val) || (root.val < p.val && root.val > q.val)) {
+            return root;
+        }
+        if ((root.val > p.val && root.val > q.val)) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if ((root.val < p.val && root.val < q.val)) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+    /**
+        Method 1 : can be used for binary tree
+        i.e. doesnot required tree to be a BST
+     */
+    public TreeNode lowestCommonAncestor_method1(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
             return root;
         }
