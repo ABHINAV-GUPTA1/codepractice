cb5ea73db33c787140d61e216bc262c7939ff6d4
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-08 12:05:11 +0530

    Time: 2 ms (97.79%), Space: 44.4 MB (66.71%)

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,31 +15,27 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+        return (isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot));
+    }
+
+    private boolean isSameTree(TreeNode root, TreeNode subRoot) {
         if (root == null && subRoot == null) {
             return true;
         }
 
-        if (root == null || subRoot == null) {
+        if (root == null || subRoot == null || root.val != subRoot.val) {
             return false;
         }
 
-        if (root.val == subRoot.val && isSameTree(root, subRoot)) {
-            return true;
-        }
-
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-    }
-
-    private boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
-        }
-        if (p == null || q == null) {
-            return false;
-        }
-        if (p.val != q.val) {
-            return false;
-        }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return (isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right));
     }
 }
