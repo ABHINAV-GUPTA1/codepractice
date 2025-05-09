b16d86ce65367960e96856f68cc9ba9e26b4e184
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-09 17:48:31 +0530

    Time: 1 ms (99.73%), Space: 44.6 MB (93.77%)

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,28 +1,25 @@
 class Solution {
-    List<List<Integer>> res;
     public List<List<Integer>> permuteUnique(int[] nums) {
-        res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        dfs(nums, new ArrayList<>(), new boolean[nums.length]);
+        dfs (nums, res, new ArrayList<>(), new boolean[nums.length]);
         return res;
     }
 
-    private void dfs(int[] arr, List<Integer> tmp, boolean[] vis) {
+    private void dfs (int[] arr, List<List<Integer>> res, List<Integer> tmp, boolean[] vis) {
         if (tmp.size() == arr.length) {
             res.add(new ArrayList<>(tmp));
             return;
         }
         for (int i = 0; i < arr.length; i++) {
-            if (vis[i] || (i > 0 && arr[i] == arr[i-1] && !vis[i-1])) {
+            if (vis[i] || (i > 0 && arr[i] == arr[i - 1] && !vis[i-1])) {
                 continue;
             }
             tmp.add(arr[i]);
             vis[i] = true;
-            dfs(arr, tmp, vis);
+            dfs(arr, res, tmp, vis);
             vis[i] = false;
-            tmp.remove(tmp.size()-1);
-            
+            tmp.remove(tmp.size() - 1);
         }
     }
-
 }
