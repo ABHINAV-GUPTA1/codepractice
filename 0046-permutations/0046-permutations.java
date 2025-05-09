e5c8005e754de7925bf52c691e4ef131f0de64bc
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-09 16:54:03 +0530

    Time: 1 ms (93.51%), Space: 44.7 MB (46.13%)

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,5 +1,42 @@
 class Solution {
+    /**
+        Method 2 : 
+            TC = O(n! * n)
+            SC = O(n)  
+     */
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int[] arr, int idx, List<Integer> tmp, List<List<Integer>> res) {
+        if (idx == arr.length) {
+            tmp.clear();
+            for (int i = 0; i < arr.length; i++) {
+                tmp.add(arr[i]);
+            }
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+        for (int i = idx; i < arr.length; i++) {
+            swap(arr, i, idx);
+            backtrack(arr, idx + 1, tmp, res);
+            swap(arr, i, idx);
+        }
+    }
+
+    private void swap(int[] arr, int a, int b) {
+        int t = arr[a];
+        arr[a] = arr[b];
+        arr[b] = t;
+    }
+    /**
+        Method 1 : 
+            TC = O(n! * n)
+            SC = O(n) + O(n) 
+     */
+    public List<List<Integer>> permute_method1(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         boolean[] vis = new boolean[nums.length];
         dfs(nums, vis, new ArrayList<>(), res);
