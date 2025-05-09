cd4f63624ae38988e21513e2528f20a684ba4478
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-09 16:35:51 +0530

    Time: 1 ms (93.51%), Space: 44.4 MB (92%)

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,58 +1,24 @@
 class Solution {
-    List<List<Integer>> res;
-    
     public List<List<Integer>> permute(int[] nums) {
-        res = new ArrayList<>();
-        dfs(0, nums);
-        return res;
-    }
-
-    private void dfs(int idx, int[] arr) {
-        if (idx == arr.length) {
-            List<Integer> tmp = new ArrayList<>();
-            for (int i = 0; i < arr.length; i++) {
-                tmp.add(arr[i]);
-            }
-            res.add(tmp);
-            return;
-        }
-
-        for (int i = idx; i < arr.length; i++) {
-            swap(arr, i, idx);
-            dfs(idx+1, arr);
-            swap(arr, i, idx);
-        }
-
-    }
-
-    private void swap(int[] arr, int a, int b) {
-        int t = arr[a];
-        arr[a] = arr[b];
-        arr[b] = t;
-    }
-
-    public List<List<Integer>> permute1(int[] nums) {
-        res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         boolean[] vis = new boolean[nums.length];
-        backtrack(nums, vis, new ArrayList<>());
+        dfs(nums, vis, new ArrayList<>(), res);
         return res;
     }
 
-    private void backtrack(int[] arr, boolean[] vis, List<Integer> tmp) {
-        
+    private void dfs(int[] arr, boolean[] vis, List<Integer> tmp, List<List<Integer>> res) {
         if (tmp.size() == arr.length) {
             res.add(new ArrayList<>(tmp));
             return;
         }
         for (int i = 0; i < arr.length; i++) {
-            if (vis[i]) {
-                continue;
+            if (!vis[i]) {
+                vis[i] = true;
+                tmp.add(arr[i]);
+                dfs(arr, vis, tmp, res);
+                tmp.remove(tmp.size() - 1);
+                vis[i] = false;
             }
-            tmp.add(arr[i]);
-            vis[i] = true;
-            backtrack(arr, vis, tmp);
-            vis[i] = false;
-            tmp.remove(tmp.size()-1);
         }
     }
 }
