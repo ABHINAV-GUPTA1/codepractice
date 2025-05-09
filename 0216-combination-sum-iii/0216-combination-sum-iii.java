2903622efbbc2c4138542a2bc376453c21ac56de
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-09 14:04:09 +0530

    Time: 0 ms (100%), Space: 41.1 MB (61.35%)

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,31 +1,29 @@
 class Solution {
-    List<List<Integer>> res;
-    int k;
     public List<List<Integer>> combinationSum3(int k, int n) {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-        res = new ArrayList<>();
-        this.k = k;
-        backtrack(0, arr, n, new ArrayList<>());
+        int[] arr = new int[]{1,2,3,4,5,6,7,8,9};
+        List<List<Integer>> res = new ArrayList<>();
+        comb(arr, 0, n, k, new ArrayList<>(), res);
         return res;
     }
 
-    void backtrack(int idx, int[] arr, int target, List<Integer> tmp) {
-        if (target < 0 || tmp.size() > k) {
+    private void comb(int[] arr, int idx, int target, int size, List<Integer> tmp, List<List<Integer>> res) {
+        if (tmp.size() > size || target < 0) {
             return;
         }
-        if (target == 0 && tmp.size() == k) {
+        if (target == 0 && tmp.size() == size) {
             res.add(new ArrayList<>(tmp));
             return;
         }
         for (int i = idx; i < arr.length; i++) {
-            if (i > idx && arr[i] == arr[i-1] || target-arr[i] < 0) {
-                continue;
+            // if (i > idx && arr[i] == arr[i - 1]) {
+            //     continue;
+            // }
+            if (arr[i] > target) {
+                return;
             }
-            tmp.add(arr[i]);
-            backtrack(i+1, arr, target-arr[i], tmp);
-            tmp.remove(tmp.size()-1);
-
+            tmp.add(arr[i]);    
+            comb(arr, i + 1, target - arr[i], size, tmp, res);
+            tmp.remove(tmp.size() - 1);
         }
-        
     }
 }
