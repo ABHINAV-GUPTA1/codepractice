bc3ce3be35ed031a224e1b3caf323f1085db6a9c
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-09 16:05:17 +0530

    Time: 1 ms (51.39%), Space: 42.9 MB (30.34%)

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+    
+    private void dfs (int[] arr, int idx, List<Integer> tmp, List<List<Integer>> res) {
+        res.add(new ArrayList<>(tmp));
+        for (int i = idx; i < arr.length; i++) {
+            tmp.add(arr[i]);
+            dfs(arr, i + 1, tmp, res);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
