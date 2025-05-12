cd34853c31818170b7217697167916f792f2fc7a
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-12 14:24:50 +0530

    Time: 134 ms (79.3%), Space: 41.7 MB (63.27%)

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,36 +1,31 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (word.charAt(0) == board[i][j]) {
-                    if (dfs(i, j, word, board, 0)) {
-                        return true;
-                    }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (word.charAt(0) == board[i][j] && dfs(board, word, i, j, 0)) {
+                    return true;
                 }
             }
-        }
+        }    
         return false;
     }
 
-    private boolean dfs(int i, int j, String word, char[][] board, int idx) {
-        if (idx >= word.length()) {
+    private boolean dfs(char[][] board, String word, int i, int j, int k) {
+        if (k == word.length()) {
             return true;
         }
-        if (i < 0 || j < 0 || i >= board.length || j >= board[i].length) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[i].length || board[i][j] != word.charAt(k)) {
             return false;
-        }
-        if (word.charAt(idx) != board[i][j]) {
-            return false;
-        }
-        char t = board[i][j];
-        board[i][j] = '0';
-        if (dfs(i+1,j, word, board, idx+1) || dfs(i,j+1, word, board, idx+1) || dfs(i,j-1, word, board, idx+1) || dfs(i-1,j, word, board, idx+1)) {
-            return true;
-        }
-        board[i][j] = t;
+        } 
+        board[i][j] = '$';
+        if (dfs(board, word, i + 1, j, k+1)
+            || dfs(board, word, i - 1, j, k+1)
+            || dfs(board, word, i, j + 1, k+1)
+            || dfs(board, word, i, j - 1, k+1)) {
+            board[i][j] = word.charAt(k);
+            return true;        
+            }
+        board[i][j] = word.charAt(k);
         return false;
     }
-
 }
