99c953ece504817476e0f27ea366cd00b613b300
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-12 14:25:31 +0530

    Time: 134 ms (79.3%), Space: 41.6 MB (63.27%)

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,5 +1,13 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        if (board == null || word == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        
+        if (word.length() == 0) {
+            return true;
+        }
+        
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (word.charAt(0) == board[i][j] && dfs(board, word, i, j, 0)) {
