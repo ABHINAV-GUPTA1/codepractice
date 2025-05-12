12a8845a9fa3d1cbf93e7f1ac93f588b0cc0dee7
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-12 12:35:13 +0530

    Time: 1 ms (94.59%), Space: 41.6 MB (18.23%)

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,24 +1,24 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        StringBuilder sb = new StringBuilder();
         List<Integer> nums = new ArrayList<>();
         int fact = 1;
         for (int i = 1; i < n; i++) {
-            fact = fact * i;
             nums.add(i);
+            fact *= i;
         }
         nums.add(n);
-        
-        k--;
+        k--; // as we will use 0 based indexing
+        StringBuilder sb = new StringBuilder();
         while (true) {
-            int idx = k / fact;
+            int idx = k/fact;
             sb.append(nums.get(idx));
             nums.remove(idx);
             if (nums.size() == 0) {
                 return sb.toString();
             }
             k = k % fact;
-            fact = fact / nums.size(); 
+            fact = fact / nums.size();
         }
+        
     }
 }
