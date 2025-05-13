5410c822a1a18e690f80dff82e36226f636f0e64
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-13 12:04:35 +0530

    Time: 4 ms (49.08%), Space: 49.9 MB (39.04%)

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,28 +3,24 @@ class Solution {
         int count = 0;
         boolean[][] vis = new boolean[grid.length][grid[0].length];
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1' && !vis[i][j]) {
-                    dfs(i, j, grid, vis);
+                    dfs(grid, i, j, vis);
                     count++;
                 }
             }
-        }
+        }    
         return count;
     }
-    int[] dx = {-1, 0,  1, 0};
-    int[] dy = {0,  -1, 0, 1};
-    private void dfs(int row, int col, char[][] grid, boolean[][] vis) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[row].length || vis[row][col] || grid[row][col] == '0') {
+
+    private void dfs (char[][] grid, int i, int j, boolean[][] vis) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || vis[i][j] || grid[i][j] != '1') {
             return;
         }
-        vis[row][col] = true;
-        int nr;
-        int nc;
-        for (int i = 0; i < dx.length; i++) {
-            nr = row + dx[i];
-            nc = col + dy[i];
-            dfs(nr, nc, grid, vis);
-        }
+        vis[i][j] = true;
+        dfs(grid, i + 1, j, vis);
+        dfs(grid, i, j + 1, vis);
+        dfs(grid, i - 1, j, vis);
+        dfs(grid, i, j - 1, vis);
     }
 }
