6ebdc1854fab90e15c99a643fd7cdaab10045ee5
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-13 10:58:51 +0530

    Time: 166 ms (87.29%), Space: 89.6 MB (87.55%)

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,61 +1,59 @@
+class TrieNode {
+    TrieNode child[];
+    boolean isEnd;
+    TrieNode() {
+        this.child = new TrieNode[26];
+    }
+}
 class WordDictionary {
-
-    Trie root;
-
+    private TrieNode root;
     public WordDictionary() {
-        root = new Trie();    
+        root = new TrieNode();
     }
     
     public void addWord(String word) {
-        insert(word);
-    }
-    
-    private void insert(String word) {
-        char[] arr = word.toCharArray();
-        Trie tmp = root;
-        for (int i = 0; i < arr.length; i++) {
-            int idx = arr[i]-'a';
+        TrieNode tmp = root;
+        int len = word.length();
+        for (int i = 0; i < len; i++) {
+            int idx = word.charAt(i) - 'a';
             if (tmp.child[idx] == null) {
-                tmp.child[idx] = new Trie();
+                tmp.child[idx] = new TrieNode();
             }
             tmp = tmp.child[idx];
         }
         tmp.isEnd = true;
     }
-
+    
     public boolean search(String word) {
-        return search(word, 0, root);
+        TrieNode tmp = root;
+        return search(tmp, word, 0);
     }
 
-    private boolean search(String word, int idx, Trie t) {
+    private boolean search(TrieNode tmp, String word, int idx) {
+        if (idx == word.length()) {
+            return tmp != null && tmp.isEnd;
+        }
+
         for (int i = idx; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (c == '.') {
-                for (int a = 0; a < 26; a++) {
-                    if (t.child[a] != null && search(word, i+1, t.child[a]) ) {
+            if (word.charAt(i) == '.') {
+                for (int j = 0; j < 26; j++) {
+                    if (tmp.child[j] == null) {
+                        continue;
+                    }
+                    if (search(tmp.child[j], word, i + 1)) {
                         return true;
                     }
                 }
                 return false;
             }
-            int idxi = c-'a';
-            if (t.child[idxi] == null) {
+            int trieIdx = word.charAt(i) - 'a';
+            if (tmp.child[trieIdx] == null) {
                 return false;
             }
-            t = t.child[idxi];
+            tmp = tmp.child[trieIdx];
         }
-        return t.isEnd;
-    }
 
-}
-
-class Trie {
-    Trie[] child;
-    boolean isEnd=false;
-
-    Trie() {
-        child = new Trie[26];
-        isEnd = false;
+        return tmp != null ? tmp.isEnd : false;
     }
 }
 
