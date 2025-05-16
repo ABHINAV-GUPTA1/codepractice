57f3a176bb2aef5dbfe50a8a0c8586609d670dcc
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-16 13:20:47 +0530

    Time: 0 ms (100%), Space: 41.2 MB (39.33%)

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,42 @@
 class Solution {
-    int[][] dp;
+
+    /**
+        Method2 : DP
+     */
     public int rob(int[] nums) {
-        dp = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++) {
-            Arrays.fill(dp[i], -1);
-        } 
-        return f(nums.length-1, 0, nums);
+        int[][] dp = new int[2][nums.length + 1];
+        dp[0][1] = nums[0];
+
+        for (int idx = 2; idx <= nums.length; idx++) {
+            for (int flag = 0; flag < 2; flag++) {
+                int take = nums[idx - 1] + dp[idx == nums.length ? 1 : flag][idx - 2];
+                int ntake = dp[flag][idx - 1];
+                dp[flag][idx] = Math.max(take, ntake);
+            }
+        }
+
+        return dp[0][nums.length];
+    }
+
+    /**
+        Method 1: recursion
+     */
+    public int rob_method1(int[] nums) {
+        return f(nums.length - 1, 0, nums);
     }
 
     int f(int idx, int flag, int[] nums) {
-        if (idx == 0) {
-            return flag == 1 ? 0 : nums[idx];
+        if (idx == 0 ) {
+            return flag == 0 ? nums[idx] : 0;
         }
-        if (idx < 0) {
+
+        if  (idx < 0) {
             return 0;
         }
-        if (dp[idx][flag] != -1) {
-            return dp[idx][flag];
-        }
-        int ntake = f(idx-1, flag, nums);
-        int take = nums[idx] + f(idx-2, idx == nums.length-1 ? 1 : flag, nums);
+        
+        int take = nums[idx] + f(idx - 2, idx == nums.length - 1 ? 1 : flag, nums);
+        int ntake = f(idx - 1, flag, nums);
 
-        return dp[idx][flag] = Math.max(take, ntake);
+        return Math.max(take, ntake);
     }
 }
