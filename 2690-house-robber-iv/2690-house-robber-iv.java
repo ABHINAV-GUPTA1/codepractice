00e2536e03bc56b235f00aeb8a1442d2f261a119
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-16 21:41:45 +0530

    Time: 20 ms (56.2%), Space: 56.1 MB (78.16%)

--- 2690-house-robber-iv/2690-house-robber-iv.java
+++ 2690-house-robber-iv/2690-house-robber-iv.java
@@ -1,35 +1,37 @@
 class Solution {
-    int MAX = (int)1e9+1;
     public int minCapability(int[] nums, int k) {
-        int low = 1;
-        int high = MAX;
-        int ans = MAX;
+        int low = 0;
+        int high = 0;
+        for (int i = 0; i < nums.length; i++) {
+            high = Math.max(high, nums[i]);
+        }
+
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (isPossible(nums, mid, k)) {
-                ans = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return ans;
+
+        return low;
     }
 
     private boolean isPossible(int[] arr, int mid, int k) {
-        int i = 0;
-        int n = arr.length;
-        while (i < n) {
-            if (arr[i] <= mid) {
+        int idx = 0;
+        while (idx < arr.length) {
+            if (arr[idx] <= mid) {
+                idx += 2;
                 k--;
-                i+=2;
             } else {
-                i++;
+                idx++;
             }
             if (k == 0) {
                 return true;
             }
         }
-        return false;
+
+        return k <= 0;
     }
 }
