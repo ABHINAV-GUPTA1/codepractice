b3e26909909aab0a6efec973f975f3ecfc29c685
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-17 13:06:04 +0530

    Time: 0 ms (100%), Space: 41.8 MB (64.13%)

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,6 +1,37 @@
 class Solution {
-
+    /**
+        Method 3: space optimized
+     */
     public int numDecodings(String s) {
+        int n = s.length();
+        // int[] dp = new int[n + 1];
+        // dp[n] = 1;
+        int prev2 = 0;
+        int prev1 = 1;
+        int curr = 0;
+        for (int idx = n - 1; idx >= 0; idx--) {
+            if (s.charAt(idx) == '0') {
+                curr = 0;
+            } else {
+                int ans = prev1;
+                if (idx + 1 < s.length()) {
+                    if (s.charAt(idx) == '1' || (s.charAt(idx) == '2' && s.charAt(idx + 1) < '7')) {
+                        ans += prev2;
+                    }
+                }
+                curr = ans;
+            }
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return curr;
+    }
+
+    /**
+        Method 2 : DP
+     */
+    public int numDecodings_method2(String s) {
         int n = s.length();
         int[] dp = new int[n + 1];
         dp[n] = 1;
@@ -21,6 +52,7 @@ class Solution {
 
         return dp[0];
     }
+
 
     /**
         Method 1 : recursion
