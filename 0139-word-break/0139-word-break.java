7f93e35bf0d2100b8ddb12518b640d065d9fe8df
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-17 23:08:10 +0530

    Time: 1 ms (99.34%), Space: 41.7 MB (95.02%)

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,68 +1,24 @@
 class Solution {
-    Trie root;
-    Boolean[] dp;
     public boolean wordBreak(String s, List<String> wordDict) {
-        root = new Trie();
-        dp = new Boolean[s.length()+1];
-        for (int i = 0; i < wordDict.size(); i++) {
-            insert(wordDict.get(i));
-        }
-        return solve(0, s);
+        return f(s, wordDict, 0, new Boolean[s.length() + 1] );
     }
 
-    private void insert(String s) {
-        char[] arr = s.toCharArray();
-        Trie tmp = root;
-        int idx;
-        for (int i = 0; i < arr.length; i++) {
-            idx = arr[i] - 'a';
-            if (tmp.child[idx] == null) {
-                tmp.child[idx] = new Trie();
-            }
-            tmp = tmp.child[idx];
+    private boolean f(String s, List<String> words, int idx, Boolean[] visit) {
+        if (visit[idx] != null) {
+            return visit[idx];
         }
-        tmp.isEnd = true;
-    }
-
-    private boolean search(String s, int start, int end) {
-        Trie tmp = root;
-        char[] arr = s.toCharArray();
-        int idx;
-        end = Math.min(end, s.length());
-        for (int i = start; i < end; i++) {
-            idx = arr[i]-'a';
-            if (tmp.child[idx] == null) {
-                return false;
-            }
-            tmp = tmp.child[idx];
-        }
-        return tmp != null ? tmp.isEnd : false;
-    }
-
-    private boolean solve(int idx, String s) {
         if (idx >= s.length()) {
-            return true;
+            return visit[idx] = true;
         }
-        if (dp[idx] != null) {
-            return dp[idx];
-        }
-
-        for (int i = idx; i < s.length(); i++) {
-            if (search(s, idx, i+1)) {
-                if (solve(i+1, s)) {
-                    return dp[idx] = true;
-                }
+        for (String word : words) {
+            if (idx <= s.length() && s.startsWith(word, idx)) {
+                if (f(s, words, idx + word.length(), visit)) {
+                    visit[idx] = true;
+                    return true;
+                }        
             }
         }
 
-        return dp[idx] = false;
-    } 
-}
-
-class Trie {
-    boolean isEnd = false;
-    Trie[] child;
-    Trie() {
-        child = new Trie[26];
+        return visit[idx] = false;
     }
 }
