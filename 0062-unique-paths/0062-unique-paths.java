bdc078f4b48f01cdc87ce78f04695457c5ae9df0
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-18 17:18:17 +0530

    Time: 0 ms (100%), Space: 41 MB (10.92%)

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,41 @@
 class Solution {
-    
+    /**
+        Method 3 : space optimised
+     */
     public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        int[] curr = new int[n];
+        int[] prev = new int[n];
+        prev[0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    curr[j] = 1;
+                    continue;
+                }
+                
+                int left = 0;
+                if (i > 0) {
+                    left = prev[j];
+                }
+                int up = 0;
+                if (j > 0) {
+                    up = curr[j - 1];
+                }
+                curr[j] =  left + up;
+            }
+            prev = Arrays.copyOf(curr, curr.length);
+        }
+
+        return prev[n - 1];
+    }
+
+    /**
+        Method 2 : DP
+     */
+
+    public int uniquePaths_method2(int m, int n) {
         int[][] dp = new int[m][n];
         dp[0][0] = 1;
         for (int i = 0; i < m; i++) {
