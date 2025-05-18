ec17de343ebf11fbad166b1576f27c97968b128d
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-18 17:16:09 +0530

    Time: 0 ms (100%), Space: 40.4 MB (75.99%)

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,36 @@
 class Solution {
-    Integer[][] dp;
+    
     public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                int left = 0;
+                if (i > 0) {
+                    left = dp[i - 1][j];
+                }
+                int up = 0;
+                if (j > 0) {
+                    up = dp[i][j - 1];
+                }
+                dp[i][j] =  left + up;
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+
+        Method 1 : recursion
+     */
+    Integer[][] dp;
+    public int uniquePaths_method1(int m, int n) {
         dp = new Integer[m][n];
         return f(m - 1, n - 1);
     }
