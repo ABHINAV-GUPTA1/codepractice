26efad49777654ddf9732fc9a6f37adf2efb3d80
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-18 12:15:12 +0530

    Time: 3 ms (79.18%), Space: 43.1 MB (64.34%)

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,6 +1,66 @@
 class Solution {
-    
+    /**
+        Method3 :trie 
+     */
+    TrieNode root;
+    Boolean[] dp;
     public boolean wordBreak(String s, List<String> wordDict) {
+        root = new TrieNode();
+        dp = new Boolean[s.length() + 1];
+        for (String word : wordDict) {
+            insert(root, word);
+        }
+        return solve(s, 0);
+    }
+
+    private boolean search(String s, int start, int end) {
+        TrieNode tmp = root;
+        for (int i = start; i < end; i++) {
+            int idx = s.charAt(i) - 'a';
+            if (tmp.child[idx] == null) {
+                return false;
+            }
+            tmp = tmp.child[idx];
+        }
+        return tmp != null && tmp.isEnd;
+    }
+
+    private boolean solve(String s, int idx) {
+        if (idx >= s.length()) {
+            return true;
+        }
+        if (dp[idx] != null) {
+            return dp[idx];
+        }
+
+        for (int i = idx; i < s.length(); i++) {
+            if (search(s, idx, i + 1)) {
+                if (solve(s, i + 1)) {
+                    return dp[idx] = true;
+                }
+            }
+        }
+        
+        return dp[idx] = false;
+    }
+
+    private void insert(TrieNode root, String s) {
+        TrieNode tmp = root;
+        for (char c: s.toCharArray()) {
+            int idx = c - 'a';
+            if (tmp.child[idx] == null) {
+                tmp.child[idx] = new TrieNode();
+            } 
+            tmp = tmp.child[idx];
+        }
+
+        tmp.isEnd = true;
+    }
+
+    /**
+        Method2: DP
+     */
+    public boolean wordBreak_method2(String s, List<String> wordDict) {
         Boolean[] visit = new Boolean[s.length() + 1];
         visit[s.length()] = true;
         for (int idx = s.length() - 1; idx >= 0; idx--) {
@@ -20,6 +80,9 @@ class Solution {
         return visit[0];
     }
 
+    /**
+        Method 1 : recursion
+     */
     public boolean wordBreak_method1(String s, List<String> wordDict) {
         return f(s, wordDict, 0, new Boolean[s.length() + 1] );
     }
@@ -40,6 +103,13 @@ class Solution {
             }
         }
 
-        return visit[idx] = false;
+        return visit[idx] = false;  
+    }
+}
+class TrieNode {
+    boolean isEnd;
+    TrieNode[] child;
+    TrieNode() {
+        child = new TrieNode[26];
     }
 }
