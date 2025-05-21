d181a1fb42aa631886184b727167bd11ce47dabd
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-21 16:37:23 +0530

    Time: 156 ms (5.35%), Space: 45 MB (27.81%)

--- 1466-jump-game-v/1466-jump-game-v.java
+++ 1466-jump-game-v/1466-jump-game-v.java
@@ -1,6 +1,38 @@
 class Solution {
-    Integer[] dp;
+
     public int maxJumps(int[] arr, int d) {
+        int res = 0;
+        // dp = new Integer[arr.length];
+        int[] dp = new int[arr.length];
+        List<Integer> index = new ArrayList<>(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            index.add(i);
+        }
+
+        index.sort((i, j) -> Integer.compare(arr[i], arr[j]));
+
+        for (int idx : index) {
+            int ans = 1;
+            for (int i = idx - 1; i >= Math.max(idx - d, 0) && arr[idx] > arr[i]; i--) {
+                ans = Math.max(ans, 1 + dp[i]);
+            }
+
+            for (int i = idx + 1; i <= Math.min(idx + d, arr.length - 1) && arr[idx] > arr[i]; i++) {
+                ans = Math.max(ans, 1 + dp[i]);
+            }
+
+            dp[idx] = ans;
+            res = Math.max(res, dp[idx]);
+        }
+        System.out.println(Arrays.toString(dp));
+        return res;
+    }
+
+    /**
+        Method 1 : Recursion + memoization
+     */
+    Integer[] dp;
+    public int maxJumps_method1(int[] arr, int d) {
         int ans = 0;
         dp = new Integer[arr.length];
         for (int i = 0; i < arr.length; i++) {
