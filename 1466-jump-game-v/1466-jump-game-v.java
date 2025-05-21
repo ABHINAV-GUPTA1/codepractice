d6509fc73e6cb9fbccf247eeced7e33a53fb4d6f
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-21 16:12:03 +0530

    Time: 13 ms (40.11%), Space: 44.7 MB (40.64%)

---
+++ 1466-jump-game-v/1466-jump-game-v.java
@@ -0,0 +1,27 @@
+class Solution {
+    Integer[] dp;
+    public int maxJumps(int[] arr, int d) {
+        int ans = 0;
+        dp = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            ans = Math.max(ans, f(arr, i, d));
+        }
+        return ans;
+    }
+
+    private int f(int[] arr, int idx, int d) {
+        if (dp[idx] != null) {
+            return dp[idx];
+        }
+        int ans = 1;
+        for (int i = idx - 1; i >= Math.max(idx - d, 0) && arr[idx] > arr[i]; i--) {
+            ans = Math.max(ans, 1 + f(arr, i, d));
+        }
+
+        for (int i = idx + 1; i <= Math.min(idx + d, arr.length - 1) && arr[idx] > arr[i]; i++) {
+            ans = Math.max(ans, 1 + f(arr, i, d));
+        }
+
+        return dp[idx] = ans;
+    }
+}
