48082054f684c1729effdb219cfb7fcddae39ae0
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-22 13:11:53 +0530

    Time: 13 ms (43.26%), Space: 51 MB (31.39%)

--- 2001-jump-game-vii/2001-jump-game-vii.java
+++ 2001-jump-game-vii/2001-jump-game-vii.java
@@ -1,5 +1,28 @@
 class Solution {
+    
     public boolean canReach(String s, int minJump, int maxJump) {
+        int farthest = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        int n = s.length();
+        while (!q.isEmpty()) {
+            int qval = q.poll();
+            int start = Math.max(qval + minJump, farthest + 1);
+            int end = Math.min(qval + maxJump + 1, n);
+            farthest = qval + maxJump;
+            for (; start < end; start++) {
+                if (s.charAt(start) == '0') {
+                    q.offer(start);
+                    if (start == n - 1) {
+                        return true;
+                    }
+                }
+            } 
+        } 
+        return false;
+    }
+
+    public boolean canReach_method1(String s, int minJump, int maxJump) {
         int n = s.length();
         boolean[] dp = new boolean[n];
         dp[0] = s.charAt(0) == '0';
