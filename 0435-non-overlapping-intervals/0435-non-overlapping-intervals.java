23fc01461c0346cf8313ab829b3f0a375f579ac7
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-25 14:52:12 +0530

    Time: 47 ms (74.13%), Space: 73.6 MB (59.22%)

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,17 +1,33 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) {
-            return 0;
+        int count = 0;
+        Arrays.sort(intervals, (a,b)-> Integer.compare(a[1], b[1]));
+        int lastTime = intervals[0][0];
+
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] < lastTime) {
+                count++;
+            } else {
+                lastTime = intervals[i][1];
+            }
         }
-        int ans = 1;
-        Arrays.sort(intervals, (a,b)->a[1]-b[1]);
+
+        return count;
+    }
+
+    public int eraseOverlapIntervals_method1(int[][] intervals) {
+        int count = 1;
+
+        Arrays.sort(intervals, (a,b)-> Integer.compare(a[1], b[1]));
         int lastTime = intervals[0][1];
+
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] >= lastTime) {
                 lastTime = intervals[i][1];
-                ans++;
+                count++;
             }
         }
-        return intervals.length-ans;
+
+        return intervals.length - count;
     }
 }
