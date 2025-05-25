8957ecd1fc04e97057c47a4697135c52ef078773
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-25 13:01:39 +0530

    Time: 8 ms (86.68%), Space: 46.4 MB (83.84%)

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,23 +1,18 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // sort the intervals
-        Arrays.sort(intervals, (a,b)-> a[0]==b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
-        // res list
-        List<int[]> list = new ArrayList<>();
-        int[] tmp; // tmp variable
+        List<int[]> res = new ArrayList<>();
+        
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        
         for (int i = 0; i < intervals.length; i++) {
-            // check if first element then insert else
-            // check last added element end time and current interval start time
-            // if curr interval start time > last added interval end time then add it else
-            // compare the endtime for both and update it in list last element  
-            if (list.size() == 0 || intervals[i][0] > list.get(list.size()-1)[1]) {
-                list.add(intervals[i]);
+            if (i == 0 || res.get(res.size() - 1)[1] < intervals[i][0]){
+                res.add(intervals[i]);
             } else {
-                tmp = list.get(list.size()-1);
+                int[] tmp = res.get(res.size() - 1);
+                tmp[0] = Math.min(tmp[0], intervals[i][0]);
                 tmp[1] = Math.max(tmp[1], intervals[i][1]);
             }
         }
-
-        return list.toArray(new int[list.size()][]);
+        return res.toArray(new int[res.size()][]);    
     }
 }
