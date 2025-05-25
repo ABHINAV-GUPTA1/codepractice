72dd3147dee9b22c125798d0b50e9c8fafe9c04b
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-25 12:28:28 +0530

    Time: 1 ms (98.84%), Space: 45.4 MB (8.52%)

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> res = new ArrayList<>();
-        Arrays.sort(intervals, (a,b)->Integer.compare(a[0], b[0]));
         for (int[] interval : intervals) {
-            if (interval[1] < newInterval[0]) {
-                res.add(interval);
-            } else if (newInterval[1] < interval[0]) {
+            if (newInterval[1] < interval[0]) {
                 res.add(newInterval);
                 newInterval = interval;
+            } else if (newInterval[0] > interval[1]) {
+                res.add(interval);
             } else {
-                newInterval[0] = Math.min(interval[0], newInterval[0]);
-                newInterval[1] = Math.max(interval[1], newInterval[1]);
+                newInterval[0] = Math.min(newInterval[0], interval[0]);
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
             }
         }
+
         res.add(newInterval);
+
         return res.toArray(new int[res.size()][]);
     }
 }
