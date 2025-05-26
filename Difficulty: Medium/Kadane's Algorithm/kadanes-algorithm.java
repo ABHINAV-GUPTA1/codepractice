cc1b6f42d6f14ac708a03f04af1116ed69cbb1c3
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-26 20:50:31 +0530

    Updated Solution for Kadane's Algorithm

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -1,58 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            Solution obj = new Solution();
-
-            // calling maxSubarraySum() function
-            System.out.println(obj.maxSubarraySum(arr));
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-/*
-    @author: Abhinav Gupta
-*/
 class Solution {
     int maxSubarraySum(int[] arr) {
+        int ans = Integer.MIN_VALUE;
         int sum = 0;
-        int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
-            if (maxSum < sum) {
-                maxSum = sum;
+            if (sum > ans) {
+                ans = sum;
             }
             if (sum < 0) {
                 sum = 0;
             }
         }
-        return maxSum;
+        return ans;
     }
 }
