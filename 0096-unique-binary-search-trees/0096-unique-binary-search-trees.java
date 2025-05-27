d2ec2ce49347ab57f8846577af3c4740d44a819b
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-27 21:24:52 +0530

    Time: 0 ms (100%), Space: 40.1 MB (89.71%)

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,41 +1,49 @@
 class Solution {
-
-    int[] dp;    
+    
     public int numTrees(int n) {
-        if (n <= 1) {
-            return 1;
-        }    
-        dp = new int[n+1];
-        /**
-            Method 1 : memoizaiton
-            Method 2 : bottom up
-         */
-        // return f(n);
-
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                dp[i] += (dp[j-1]*dp[i-j]);
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int idx = 2; idx <= n; idx++) {
+            int ans = 0;
+            for (int i = 1; i <= idx; i++) {
+                ans += dp[i-1] * dp[idx - i];
             }
-        }
-        return dp[n];
+            dp[idx] = ans;
+        }   
 
+        return dp[n];
+        
     }
 
-    private int f(int x) {
-        if (x <= 1) {
+    public int numTrees_method2(int n) {
+        return (int) (factorial(2 * n) / (factorial(n + 1) * factorial(n)) );
+    }
+
+    private long factorial(int n) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans *= i;
+        }
+        return ans;
+    }
+    
+    Integer[] dp;
+    public int numTrees_method1(int n) {
+        dp = new Integer[n + 1];
+        return f(n);
+    }
+
+    private int f(int n) {
+        if (n < 2) {
             return 1;
         }
-        if (dp[x] != 0) {
-            return dp[x];
+        if (dp[n] != null) {
+            return dp[n];
         }
         int ans = 0;
-        for (int i = 1; i <= x; i++) {
-            ans += f(i-1)*f(x-i);
+        for (int i = 1; i <= n; i++) {
+            ans += f(i-1) * f(n - i);
         }
-
-        return dp[x] = ans;
+        return dp[n] = ans;
     }
 }
