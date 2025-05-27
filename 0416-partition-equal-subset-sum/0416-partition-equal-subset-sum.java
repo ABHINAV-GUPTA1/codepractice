ba05b6f87964f8fc143bfa37418c35b403d234c2
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-27 13:36:55 +0530

    Time: 43 ms (46.38%), Space: 45 MB (53.14%)

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -13,6 +13,41 @@ class Solution {
         }
         
         sum /= 2;
+        
+        boolean[] prev = new boolean[sum + 1];
+        boolean[] curr = new boolean[sum + 1];
+        prev[0] = true;
+        if (nums[0] <= sum) {
+            prev[nums[0]] = true;
+        }
+        for (int idx = 1; idx < nums.length; idx++) {
+            for (int k = 1; k <= sum; k++) {
+                boolean ntake = prev[k];
+                boolean take = false;
+                if (k >= nums[idx]) {
+                    take = prev[k - nums[idx]];
+                }
+
+                curr[k] = take || ntake;
+            }
+            prev = Arrays.copyOf(curr, curr.length);
+        }
+        return prev[sum];
+    }
+
+    public boolean canPartition_method2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int sum = 0;
+        
+        sum = Arrays.stream(nums).sum();
+        
+        if (sum % 2 == 1) {
+            return false;
+        }
+        
+        sum /= 2;
         boolean[][] dp = new boolean[nums.length][sum + 1];
         for (int i = 0; i < nums.length; i++) {
             dp[i][0] = true; 
