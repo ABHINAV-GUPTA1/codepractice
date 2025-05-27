22868fcffdb056ede1eeb86e8796eea0f2c25301
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-27 11:08:28 +0530

    Time: 795 ms (5.12%), Space: 62.1 MB (67.56%)

---
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        Queue<int[]> q = new LinkedList<>();
+        int res = 0;
+        List<Integer>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            if (adj[i] == null) {
+                adj[i] = new ArrayList<>();
+            }
+            if (manager[i] == -1) {
+                continue;
+            }
+            if (adj[manager[i]] == null) {
+                adj[manager[i]] = new ArrayList<>();
+            }
+            adj[manager[i]].add(i);
+        }
+
+        q.offer(new int[]{headID, 0});
+        while (!q.isEmpty()) {
+            int[] arr = q.poll();
+            int u = arr[0];
+            int time = arr[1];
+            res = Math.max(res, time);
+            System.out.println(u);
+            for (int v : adj[u]) {
+                q.offer(new int[]{v, time + informTime[u]});
+            }
+        }
+        return res;
+    }
+}
