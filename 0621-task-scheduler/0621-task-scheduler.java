bb7b5fedca6c1acf69742578656edf0f71051632
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-27 17:27:57 +0530

    Time: 22 ms (53.81%), Space: 45.2 MB (95.71%)

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,66 +1,32 @@
 class Solution {
-    /**
-        Method 1
-     */
-    public int leastInterval1(char[] tasks, int n) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        List<Integer> tlist;
-        Map<Character, Integer> map = new HashMap<>();
-        int ans = 0;
-
+    public int leastInterval(char[] tasks, int n) {
+        Queue<int[]> q = new LinkedList<>();
+        int[] map = new int[26];
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
         for (int i = 0; i < tasks.length; i++) {
-            map.put(tasks[i], map.getOrDefault(tasks[i], 0)+1);
+            map[tasks[i] - 'A']++;
         }
-
-        for (int val : map.values()) {
-            pq.offer(val);
-        }
-
-        while (!pq.isEmpty()) {
-            tlist = new ArrayList<>();
-            for (int i = 1; i <= n+1 && !pq.isEmpty(); i++) {
-                tlist.add(pq.poll()-1);
-                
+        for (int i = 0; i < 26; i++) {
+            if (map[i] != 0) {
+                pq.offer(map[i]);
             }
-
-            for (int val : tlist) {
+        }
+        int time = 0;
+        while (!pq.isEmpty() || !q.isEmpty()) {
+            time += 1;
+            if (!pq.isEmpty()) {
+                int val = pq.poll();
+                val--;
                 if (val > 0) {
-                    pq.offer(val);
+                    q.offer(new int[]{val, time + n});
                 }
             }
-
-            if (pq.isEmpty()) {
-                ans += tlist.size();
-            } else {
-                ans += (n+1);
+            if (!q.isEmpty() && q.peek()[1] == time) {
+                int[] val = q.poll();
+                pq.offer(val[0]);
             }
         }
 
-        return ans;
+        return time;
     }
-
-    /**
-        Method 2
-     */
-
-    public int leastInterval(char[] tasks, int n) {
-        int ans = 0;
-        int[] map = new int[26];
-        for (int i = 0; i < tasks.length; i++) {
-            map[tasks[i]-'A']++;
-        }
-        Arrays.sort(map);
-        ans = map[25];
-        int  spaceCnt = ans-1;
-        int totalSpaces = spaceCnt * n;
-        for (int i = 24; i >= 0; i--) {
-            totalSpaces -= Math.min(map[i], spaceCnt);
-        }
-        if (totalSpaces > 0) {
-            return tasks.length + totalSpaces;
-        }
-
-        return tasks.length;
-    }
-
 }
