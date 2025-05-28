b49cb987f6836a1bd62ee93857f06af5305bb561
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-05-28 10:11:21 +0530

    Time: 1 ms (98.73%), Space: 44.6 MB (59.85%)

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return generate(1, n);
+    }
+
+    private List<TreeNode> generate(int left, int right) {
+        List<TreeNode> res = new ArrayList<>();
+        if (left > right) {
+            res.add(null);
+            return res;
+        }
+        for (int i = left; i <= right; i++) {
+            List<TreeNode> leftNodes = generate(left, i - 1);
+            List<TreeNode> rightNodes = generate(i + 1, right);
+            for (TreeNode leftNode : leftNodes) {
+                for (TreeNode rightNode : rightNodes) {
+                    res.add(new TreeNode(i, leftNode, rightNode));
+                }
+            }
+        }
+
+        return res;
+    }
+}
