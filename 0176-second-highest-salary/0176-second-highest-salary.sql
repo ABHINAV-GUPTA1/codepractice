1cd3fc28afbb0886fe6ceeaafa658efe9e79cfc6
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-07-23 21:55:43 +0530

    Time: 689 ms (5%), Space: 0B (100%)

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,9 +1,4 @@
 # Write your MySQL query statement below
+#SELECT MAX(salary) as SecondHighestSalary FROM Employee WHERE salary <> (SELECT MAX(salary) FROM Employee);
 
-#SELECT MAX(salary) AS SecondHighestSalary FROM Employee 
-#WHERE salary 
-#< (SELECT MAX(salary) FROM Employee);
-
-SELECT (SELECT DISTINCT(salary) FROM Employee
-ORDER BY salary DESC
-LIMIT 1,1) AS SecondHighestSalary;
+SELECT (SELECT DISTINCT(salary) FROM Employee ORDER BY salary desc LIMIT 1,1) As SecondHighestSalary;
