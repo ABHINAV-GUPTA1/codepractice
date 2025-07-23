231013fddd73c1d79b39d991d42ae77d617e7782
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-07-23 21:21:52 +0530

    Time: 36 ms (86.99%), Space: 45.4 MB (87.67%)

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -1,8 +1,45 @@
 class Solution {
     /**
-        Method 1 : Using stack
+        Method 2: using 2 pointers
      */
     public int maximumGain(String s, int x, int y) {
+        String max;
+        String min;
+        if (x > y) {
+            String tmpStr = findScoreOptimised(s, "ab");
+            int ans = ((s.length() - tmpStr.length()) / 2) * x;
+
+            String tmpStr2 = findScoreOptimised(tmpStr, "ba");
+            ans += ((tmpStr.length() - tmpStr2.length()) / 2) * y;
+            return ans;
+        }
+        String tmpStr = findScoreOptimised(s, "ba");
+        int ans = ((s.length() - tmpStr.length()) / 2) * y;
+
+        String tmpStr2 = findScoreOptimised(tmpStr, "ab");
+        ans += ((tmpStr.length() - tmpStr2.length()) / 2) * x;
+        return ans;
+    }
+
+    private String findScoreOptimised(String s, String pattern) {
+        int i = 0;
+        char[] arr = s.toCharArray();
+        for (int j = 0; j < arr.length; j++) {
+            arr[i] = arr[j];
+            i++;
+            if (i > 1 && arr[i-2] == pattern.charAt(0) && arr[i - 1] == pattern.charAt(1)) {
+                i -= 2;
+            }
+        }
+
+        return new String(arr).substring(0, i);
+
+    }
+    
+    /**
+        Method 1 : Using stack
+     */
+    public int maximumGain_method1(String s, int x, int y) {
         String max;
         String min;
         if (x > y) {
