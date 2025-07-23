3864f6dd3315fd8b3f5c55c9de1eb04e6071232e
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-07-23 22:00:57 +0530

    Time: 460 ms (35.6%), Space: 0B (100%)

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,8 +1,8 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  SET N = N-1;
+    SET N = N-1;
   RETURN (
-      SELECT (SELECT DISTINCT(salary) FROM Employee ORDER BY salary DESC LIMIT N,1) AS salary
-
+      # Write your MySQL query statement below.
+     SELECT (SELECT DISTINCT(salary) FROM Employee ORDER BY salary DESC LIMIT N, 1) AS salary
   );
 END
