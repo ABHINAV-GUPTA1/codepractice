4e032dc7f99a323ed866be1b7f4ba30495f3c456
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2025-07-23 21:17:10 +0530

    Time: 255 ms (13.01%), Space: 47.7 MB (15.75%)

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -1,68 +1,41 @@
 class Solution {
-    String str;
+    /**
+        Method 1 : Using stack
+     */
     public int maximumGain(String s, int x, int y) {
-        String s1 = "ab";
-        String s2 = "ba";
-        if (x < y) {
-            int t = x;
-            x = y;
-            y = t;
-            
-            String ts = s1;
-            s1 = s2;
-            s2 = ts;
-        }    
-        str = s;
-        return f(s1, x) + f(s2, y);
-    }
+        String max;
+        String min;
+        if (x > y) {
+            String tmpStr = findScore(s, "ab");
+            int ans = ((s.length() - tmpStr.length()) / 2) * x;
 
-    private int f(String req, int val) {
-        String s = str;
-        int read = 0;
-        int write = 0;
-        int ans = 0;
-        int n = str.length();
-        char[] arr = new char[n];
-        while (read < n) {
-            arr[write++] = s.charAt(read);
-            if (write > 1 && arr[write-2] == req.charAt(0) && arr[write-1] == req.charAt(1)) {
-                write -= 2;
-                ans += val;
-            }
-            read++;
+            String tmpStr2 = findScore(tmpStr, "ba");
+            ans += ((tmpStr.length() - tmpStr2.length()) / 2) * y;
+            return ans;
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < write; i++) {
-            sb.append(arr[i]);
-        }
-        str = sb.toString();
+        String tmpStr = findScore(s, "ba");
+        int ans = ((s.length() - tmpStr.length()) / 2) * y;
+
+        String tmpStr2 = findScore(tmpStr, "ab");
+        ans += ((tmpStr.length() - tmpStr2.length()) / 2) * x;
         return ans;
     }
 
-    private int f1(String req, int val) {
-        String s = str;
+    private String findScore(String str, String pattern) {
         Stack<Character> st = new Stack<>();
-        int ans = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (st.isEmpty()) {
-                st.push(s.charAt(i));
+        for (int i = 0; i < str.length(); i++) {
+            if (!st.isEmpty() && st.peek() == pattern.charAt(0) && str.charAt(i) == pattern.charAt(1)) {
+                st.pop();
             } else {
-                if (st.peek() == req.charAt(0) && s.charAt(i) == req.charAt(1)) {
-                    st.pop();
-                    ans += val;
-                } else {
-                    st.push(s.charAt(i));
-                }
+                st.push(str.charAt(i));
             }
         }
+
         StringBuilder sb = new StringBuilder();
         while (!st.isEmpty()) {
-            sb.insert(0, st.pop());
+            sb.append(st.pop());
         }
-
-        str = sb.toString();
-        return ans;
+        return sb.reverse().toString();
+        
     }
-
-
 }
