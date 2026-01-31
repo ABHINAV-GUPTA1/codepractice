0445d1aba81cc844a3716ca4d07e1da9df123d73
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2026-01-31 19:19:21 +0530

    Time: 0 ms (100%), Space: 20.8 MB (25.32%)

---
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        if target >= letters[-1] or target < letters[0]:
+            return letters[0]
+        left = 0
+        right = len(letters) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if letters[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return letters[left]
+        
