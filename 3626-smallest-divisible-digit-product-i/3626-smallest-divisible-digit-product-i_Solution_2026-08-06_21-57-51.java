dd51213b3af8b2c7c885038882d7fc8a7f3af73f
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2026-08-06 22:03:10 +0530

    Update 3626-smallest-divisible-digit-product-i_Solution_2026-08-06_21-57-51.java

--- 3626-smallest-divisible-digit-product-i/3626-smallest-divisible-digit-product-i_Solution_2026-08-06_21-57-51.java
+++ 3626-smallest-divisible-digit-product-i/3626-smallest-divisible-digit-product-i_Solution_2026-08-06_21-57-51.java
@@ -1,5 +1,21 @@
 class Solution {
     public int smallestNumber(int n, int t) {
-        return (((n % 10) % t) == 0) ? n : n + 1;
+        while (!isPossible(n, t)) {
+            n++;
+        }
+
+        return n;
+    }
+
+    private boolean isPossible(int n, int t) {
+        int prod = 1;
+        while (n > 0) {
+            prod *= (n % 10);
+            if (prod == 0) {
+                break;
+            }
+            n /= 10;
+        }
+        return (prod % t) == 0;
     }
 }
