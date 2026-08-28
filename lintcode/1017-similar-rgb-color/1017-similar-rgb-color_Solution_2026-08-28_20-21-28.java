8e8d432a429f08e3d17dcd1d0dd18d8e04795850
ABHINAV GUPTA <ABHIG605@GMAIL.COM>
2026-08-28 20:21:28 +0530

    Add LintCode solution

---
+++ lintcode/1017-similar-rgb-color/1017-similar-rgb-color_Solution_2026-08-28_20-21-28.java
@@ -0,0 +1,9 @@
+public class Solution {
+    /**
+     * @param color: the given color
+     * @return: a 7 character color that is most similar to the given color
+     */
+    public String similarRGB(String color) {
+        // Write your code here
+    }
+}
